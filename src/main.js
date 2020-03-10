dbd146478852feae8f2317075aa6ad05e832b1fc
Carranza94 <jgca9417@gmail.com>
2020-03-10 12:54:31 -0500

    createElement comentario

--- src/main.js
+++ src/main.js
@@ -17,3 +17,25 @@ for (let i = 0; i < data.pokemon.length; i += 1) {
 }
 
 // console.log(example, data);
+/*
+for (let i = 0; i < data.pokemon.length; i += 1) {
+    // console.log(element.name, element.num, element.img);
+    // console.log(element.generation.name);
+    if (data.pokemon[i].generation.name === 'kanto') {
+      // Agregando imagen
+      const image = document.createElement('img');
+      image.setAttribute('src', data.pokemon[i].img);
+      /*
+      todos los datos
+      const pokemon = [data.pokemon[i].name, data.pokemon[i].num];
+      const newDiv = document.createElement('div');
+      newDiv.innerHTML += pokemon;
+     
+      // console.log(data.pokemon.name, data.pokemon.num, data.pokemon.img);
+      document.querySelector('.generation-I').appendChild(newDiv);
+      // newDiv.appendChild(pokemon);
+      document.querySelector('.generation-I').appendChild(image);
+      // console.log(newDiv);
+    }
+  }
+  */
