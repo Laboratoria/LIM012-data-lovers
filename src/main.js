a7bfacf2e0f445c707a4e2d370d31224ad2d51b1
mariebec <jmariebc@gmail.com>
2020-03-10 11:12:07 -0500

    html y datos con template string

--- src/main.js
+++ src/main.js
@@ -1,6 +1,36 @@
-import { example } from './data.js';
-// import data from './data/atletas/atletas.js';
-// import data from './data/lol/lol.js';
+// import { example } from './data.js';
 import data from './data/pokemon/pokemon.js';
 
-console.log(example, data);
+const johto = document.querySelector('.generation-II');
+
+for (let i = 0; i < data.pokemon.length; i += 1) {
+  if (data.pokemon[i].generation.name === 'johto') {
+    const pokemon = `
+          <div>
+              <p>${data.pokemon[i].num}</p>
+              <img src = "${data.pokemon[i].img}">
+              <p>${data.pokemon[i].name}</p> 
+          </div>
+      `;
+    johto.innerHTML += pokemon;
+  }
+}
+
+// console.log(example, data);
+//
+//
+// for (let element of data.pokemon) {
+//   // console.log(element.name, element.num, element.img);
+//   // console.log(element.generation.name);
+//   if (element.generation.name === 'johto') {
+//     const pokemon = `
+//         <div>
+//             <p>${element.num}</p>
+//             <img src = "${element.img}">
+//             <p>${element.name}</p>
+//         </div>
+//     `;
+//   johto.innerHTML += pokemon;
+//   // console.log(element.name, element.num, element.img);
+//   }
+// }
