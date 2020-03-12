f44248c9a1085583400b7c797eb801c17f62cdad
katherinChi <kathychi02129@gmail.com>
2020-03-12 12:28:20 -0500

    primer avance

--- src/main.js
+++ src/main.js
@@ -1,10 +1,38 @@
-import { example } from './data.js';
-// import data from './data/injuries/injuries.js';
-// import data from './data/lol/lol.js';
-// import data from './data/patient/patient.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
-// import data from './data/steam/steam.js';
-// import data from './data/worldbank/worldbank.js';
 
-console.log(example, data);
+import data from './data/pokemon/pokemon.js';
+
+const pokemones=data.pokemon;
+const inputSearchElem=document.getElementById("inputSearch");
+const searchIconElem=document.getElementById("searchIcon");
+
+const screen1Elem=document.getElementById('screen1');
+const screen2Elem=document.getElementById('screen2');
+
+
+
+
+searchIconElem.addEventListener('click',()=>{
+    screen1Elem.style.display="none";
+    screen2Elem.style.display="block"
+   
+    screen2Elem.innerHTML=` Hay ${pokemones.length} pokemones
+    ${pokemones.map(showNum).join('')}
+`
+
+});
+    
+const showNum=(poke)=>{
+    const inputSValue=inputSearchElem.value;
+    if(inputSValue===poke.name){
+    return `
+        <section id="s2img">
+            <span>${poke.num}</span>
+            <img src="${poke.img}" alt="pokemonImage">
+            <span id="namePok"> ${poke.name}</span>
+        </section>
+    `
+    }
+    else{
+        console.log("error");
+    }
+}
