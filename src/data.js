73b569e348559f19fc3b19cfc43234e2dfeaee2c
karen <karengvcs@gmail.com>
2020-03-13 16:14:39 -0500

    corrigiendo el eslint

--- src/data.js
+++ src/data.js
@@ -1,17 +1,15 @@
 import data from './data/pokemon/pokemon.js';
 
 export const dinamicSearchPokemon = (wordIntroduced) => {
-  const pokemonFilter=[];
-  for (let i =0; i<data.pokemon.length; i++){
-  let word = data.pokemon[i].name.substring(0, wordIntroduced.length);
-    if( wordIntroduced.length <= data.pokemon[i].name.length && wordIntroduced.length!= 0){
-      if(wordIntroduced.toLowerCase() == word.toLowerCase()){
-         pokemonFilter.push(data.pokemon[i]);
+  const pokemonFilter = [];
+  let word;
+  for (let i = 0; i < data.pokemon.length; i++) {
+    word = data.pokemon[i].name.substring(0, wordIntroduced.length);
+    if (wordIntroduced.length <= data.pokemon[i].name.length && wordIntroduced.length !== 0) {
+      if (wordIntroduced.toLowerCase() === word.toLowerCase()) {
+        pokemonFilter.push(data.pokemon[i]);
       }
     }
   }
   return pokemonFilter;
-}
-
-
-
+};
--- src/main.js
+++ src/main.js
@@ -1,86 +1,84 @@
 import { dinamicSearchPokemon } from './data.js';
-import data from './data/pokemon/pokemon.js';
-import pokemon from './data/pokemon/pokemon.js';
-const colors = { "normal" : "#AAA284", "fire" : "#FF9C06", "grass": "#95C215",
-                "water": "#168CBF","electric":"#F4C313","ice" : "#85EFF5",
-                "flying" : "#20E5F1","poison" : "#C22AF8","ground" : "#AB8709",
-                "psychic" : "#F013F4","rock" : "#C9AF53","bug" : "#4C6C2C",
-                "dragon" : "#731E9B","ghost" : "#8968AA","dark" : "#6C695C",
-                "steel" : "#9DAFC0","fairy" : "#FAB3E2","fighting" : "#BD5022",
-                };
-                
-const createPokemonCard= (index,dataPokemon)=>{
-  let divCard=document.createElement("div")
-    divCard.className="pokemon-card";
-    document.getElementById("card-container").appendChild(divCard);
-    let divCardName=document.createElement("div");
-    let divCardImage=document.createElement("div");
-    let divCardTypes=document.createElement("div");
-    divCardName.className="pokemon-name";
-    divCardImage.className="pokemon-image";
-    divCardTypes.className="pokemon-types";
-    document.getElementsByClassName("pokemon-card")[index].appendChild(divCardName);
-    document.getElementsByClassName("pokemon-card")[index].appendChild(divCardImage);
-    document.getElementsByClassName("pokemon-card")[index].appendChild(divCardTypes);
-    putPokemonName(dataPokemon.name, divCardName);
-    putPokemonImage(dataPokemon.img, divCardImage);
-    putPokemonTypes(dataPokemon.type, divCardTypes);
-}
+// import data from './data/pokemon/pokemon.js';
+
+const colors = {
+  normal: '#AAA284',
+  fire: '#FF9C06',
+  grass: '#95C215',
+  water: '#168CBF',
+  electric: '#F4C313',
+  ice: '#85EFF5',
+  flying: '#20E5F1',
+  poison: '#C22AF8',
+  ground: '#AB8709',
+  psychic: '#F013F4',
+  rock: '#C9AF53',
+  bug: '#4C6C2C',
+  dragon: '#731E9B',
+  ghost: '#8968AA',
+  dark: '#6C695C',
+  steel: '#9DAFC0',
+  fairy: '#FAB3E2',
+  fighting: '#BD5022',
+};
+
 
 const createPokemonType = (type) => {
-  const divPokemonType = document.createElement("div");
-  divPokemonType.className = "pokemon-type";  
+  const divPokemonType = document.createElement('div');
+  divPokemonType.className = 'pokemon-type';
   divPokemonType.appendChild(document.createTextNode(type));
   divPokemonType.style.background = colors[type];
   return divPokemonType;
-}
+};
 
- const putPokemonImage =(dataImgPokemon, divCardImage) =>{
-    let img = document.createElement("IMG");
-    img.id = "img-pokemon";
-    img.setAttribute("src",dataImgPokemon);
-    img.setAttribute("alt", "Imagen Pokemón");
-    divCardImage.appendChild(img);
- }
+const putPokemonImage = (dataImgPokemon, divCardImage) => {
+  const img = document.createElement('IMG');
+  img.id = 'img-pokemon';
+  img.setAttribute('src', dataImgPokemon);
+  img.setAttribute('alt', 'Imagen Pokemón');
+  divCardImage.appendChild(img);
+};
 
- const putPokemonName = (dataNamePokemon, divCardName) => {
-    divCardName.textContent=dataNamePokemon;
-  }
+/* eslint no-param-reassign: ["error", { "props": false }] */
+const putPokemonName = (dataNamePokemon, divCardName) => {
+  divCardName.textContent = dataNamePokemon;
+};
 
 const putPokemonTypes = (dataTypesPokemon, divCardTypes) => {
-  for(let i = 0; i < dataTypesPokemon.length; i++) {
+  for (let i = 0; i < dataTypesPokemon.length; i += 1) {
     divCardTypes.appendChild(createPokemonType(dataTypesPokemon[i]));
   }
-}
- const showCard = ()=> {
-    for (let i=0; i<data.pokemon.length; i++){
-        createPokemonCard(i,data.pokemon[i]);
-    }
+};
+
+const createPokemonCard = (index, dataPokemon) => {
+  const divCard = document.createElement('div');
+  divCard.className = 'pokemon-card';
+  document.getElementById('card-container').appendChild(divCard);
+  const divCardName = document.createElement('div');
+  const divCardImage = document.createElement('div');
+  const divCardTypes = document.createElement('div');
+  divCardName.className = 'pokemon-name';
+  divCardImage.className = 'pokemon-image';
+  divCardTypes.className = 'pokemon-types';
+  document.getElementsByClassName('pokemon-card')[index].appendChild(divCardName);
+  document.getElementsByClassName('pokemon-card')[index].appendChild(divCardImage);
+  document.getElementsByClassName('pokemon-card')[index].appendChild(divCardTypes);
+  putPokemonName(dataPokemon.name, divCardName);
+  putPokemonImage(dataPokemon.img, divCardImage);
+  putPokemonTypes(dataPokemon.type, divCardTypes);
+};
+
+const showCard = (dataPokemon) => {
+  for (let i = 0; i < dataPokemon.length; i += 1) {
+    createPokemonCard(i, dataPokemon[i]);
   }
+};
 
-
-const createPokemonType = (type) => {
-  const divPokemonType = document.createElement("div");
-  divPokemonType.className = "pokemon-type";  
-  divPokemonType.appendChild(document.createTextNode(type));
-  divPokemonType.style.background = colors[type];
-  return divPokemonType;
-}
-
-
-const showCard = (dataPokemon)=> {
-    for (let i=0; i<dataPokemon.length; i++){
-        createPokemonCard(i,dataPokemon[i]);
-    }
-  }
-
-const searchPokemon = ()=>{
-  document.getElementById("card-container").innerHTML ="";
-  let wordIntroduced=document.getElementById("input-bar-search").value;
+const searchPokemon = () => {
+  document.getElementById('card-container').innerHTML = '';
+  const wordIntroduced = document.getElementById('input-bar-search').value;
   showCard(dinamicSearchPokemon(wordIntroduced));
-}
+};
 
-//*******************************************************************
-const inputBarSearch= document.getElementById("input-bar-search");
-inputBarSearch.addEventListener('keyup',searchPokemon);
-
+const inputBarSearch = document.getElementById('input-bar-search');
+inputBarSearch.addEventListener('keyup', searchPokemon);
