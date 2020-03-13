d9d4472ba4e38ae0f6e0aea80e01426177d41d4e
Carranza94 <jgca9417@gmail.com>
2020-03-13 17:12:42 -0500

    Botón hacia arriba

--- src/main.js
+++ src/main.js
@@ -5,14 +5,14 @@ const kanto = document.getElementById('kanto');
 const johto = document.getElementById('johto');
 
 for (let i = 0; i < data.pokemon.length; i += 1) {
-  if (data.pokemon[i].generation.name === 'kanto'){
+  if (data.pokemon[i].generation.name === 'kanto') {
     const pokemon = `
         <div class="pokemon-card">
             <p class="pokemon-number left">${data.pokemon[i].num}</p>
             <img class="pokemon-image" src = "${data.pokemon[i].img}">
             <p class="pokemon-name">${data.pokemon[i].name}</p> 
         </div>
-      `;   
+      `;
     kanto.innerHTML += pokemon;
   } else {
     const pokemon = `
@@ -25,6 +25,23 @@ for (let i = 0; i < data.pokemon.length; i += 1) {
     johto.innerHTML += pokemon;
   }
 }
+
+window.onscroll = () => {
+  if (document.documentElement.scrollTop > 100) {
+    document.getElementsByTagName('div')[0].classList.add('show');
+  } else {
+    document.getElementsByTagName('div')[0].classList.remove('show');
+  }
+};
+
+document.querySelector('.go-top-container').addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+});
+
+
 //
 // console.log(data);
 /*
