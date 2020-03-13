8ddb45f7b9eb324e123dbb4746dac690cc7afe46
karen <karengvcs@gmail.com>
2020-03-13 13:18:55 -0500

    actualizaciones

--- src/main.js
+++ src/main.js
@@ -8,8 +8,16 @@ import pokemon from './data/pokemon/pokemon.js';
 // import data from './data/steam/steam.js';
 // import data from './data/worldbank/worldbank.js';
 
-const createCard= (i,dataNamePokemon,dataImgPokemon)=>{
-    let divCard=document.createElement("div")
+const colors = { "normal" : "#AAA284", "fire" : "#FF9C06", "grass": "#95C215",
+                "water": "#168CBF","electric":"#F4C313","ice" : "#85EFF5",
+                "flying" : "#20E5F1","poison" : "#C22AF8","ground" : "#AB8709",
+                "psychic" : "#F013F4","rock" : "#C9AF53","bug" : "#4C6C2C",
+                "dragon" : "#731E9B","ghost" : "#8968AA","dark" : "#6C695C",
+                "steel" : "#9DAFC0","fairy" : "#FAB3E2","fighting" : "#BD5022",
+                };
+                
+const createPokemonCard= (index,dataPokemon)=>{
+  let divCard=document.createElement("div")
     divCard.className="pokemon-card";
     document.getElementById("card-container").appendChild(divCard);
     let divCardName=document.createElement("div");
@@ -18,38 +26,39 @@ const createCard= (i,dataNamePokemon,dataImgPokemon)=>{
     divCardName.className="pokemon-name";
     divCardImage.className="pokemon-image";
     divCardTypes.className="pokemon-types";
-    document.getElementsByClassName("pokemon-card")[i].appendChild(divCardName);
-    document.getElementsByClassName("pokemon-card")[i].appendChild(divCardImage);
-    document.getElementsByClassName("pokemon-card")[i].appendChild(divCardTypes);
-    putName(dataNamePokemon, divCardName);
-    putImage(dataImgPokemon, divCardImage);
+    document.getElementsByClassName("pokemon-card")[index].appendChild(divCardName);
+    document.getElementsByClassName("pokemon-card")[index].appendChild(divCardImage);
+    document.getElementsByClassName("pokemon-card")[index].appendChild(divCardTypes);
+    putPokemonName(dataPokemon.name, divCardName);
+    putPokemonImage(dataPokemon.img, divCardImage);
+    putPokemonTypes(dataPokemon.type, divCardTypes);
 }
- const putImage =(dataImgPokemon, divCardImage) =>{
-    let img=document.createElement("IMG");
+ const putPokemonImage =(dataImgPokemon, divCardImage) =>{
+    let img = document.createElement("IMG");
+    img.id = "img-pokemon";
     img.setAttribute("src",dataImgPokemon);
     img.setAttribute("alt", "Imagen Pokemón");
     divCardImage.appendChild(img);
  }
 
- const putName = (dataNamePokemon, divCardName) => {
+ const putPokemonName = (dataNamePokemon, divCardName) => {
     divCardName.textContent=dataNamePokemon;
   }
+
+const putPokemonTypes = (dataTypesPokemon, divCardTypes) => {
+  console.log(dataTypesPokemon);
+  for(let i = 0; i < dataTypesPokemon.length; i++) {
+    divCardTypes.appendChild(createPokemonType(dataTypesPokemon[i]));
+  }
+}
  const showCard = ()=> {
     for (let i=0; i<data.pokemon.length; i++){
-        createCard(i,data.pokemon[i].name,data.pokemon[i].img);
+        createPokemonCard(i,data.pokemon[i]);
     }
-
   }
 
-const colors = { "normal" : "#AAA284", "fire" : "#FF9C06", "grass": "#95C215",
-                "water": "#168CBF","electric":"#F4C313","ice" : "#85EFF5",
-                "flying" : "#20E5F1","poison" : "#C22AF8","ground" : "#AB8709",
-                "psychic" : "#F013F4","rock" : "#C9AF53","bug" : "#4C6C2C",
-                "dragon" : "#731E9B","ghost" : "#8968AA","dark" : "#6C695C",
-                "steel" : "#9DAFC0","fairy" : "#FAB3E2","fighting" : "#BD5022",
-                };
 
-const createElementPokemonType = (type) => {
+const createPokemonType = (type) => {
   const divPokemonType = document.createElement("div");
   divPokemonType.className = "pokemon-type";  
   divPokemonType.appendChild(document.createTextNode(type));
@@ -57,8 +66,7 @@ const createElementPokemonType = (type) => {
   return divPokemonType;
 }
 showCard();
-//const PokemonTypes = document.getElementsByClassName("PokemonType");
-//PokemonTypes[0].appendChild(createElementPokemonType("bug"));
+
 
 const searchPokemon = ()=>{
   document.getElementById("card-container").innerHTML ="";
