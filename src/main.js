f41325adf4b9674036505b40bfe2dddf2b35559b
luzcry <tu-email@gmail.com>
2020-03-13 08:51:07 -0500

    areglos css

--- src/main.js
+++ src/main.js
@@ -7,12 +7,12 @@ primeraInterfaz.style.display = "block";
 segundaInterfaz.style.display = "none";
 
 document.getElementById("buttonEnter").addEventListener("click", () => {
-    const name = document.getElementById("inputName").value;
-    const newStr = `${name[0].toUpperCase()}${name.slice(1)}`;
-    const parrafo = document.getElementById("nombreIngresado");
-    parrafo.innerHTML = `!\n Welcome ${newStr}\n !`;
-    segundaInterfaz.style.display = "block";
-    primeraInterfaz.style.display = "none";
+  const name = document.getElementById("inputName").value;
+  const newStr = `${name[0].toUpperCase()}${name.slice(1)}`;
+  const parrafo = document.getElementById("nombreIngresado");
+  parrafo.innerHTML = `!\n Welcome ${newStr}\n !`;
+  segundaInterfaz.style.display = "block";
+  primeraInterfaz.style.display = "none";
 });
 
 /*Muestra de la lista de campeones*/
@@ -41,51 +41,51 @@ let filteredList = document.querySelector("#filteredList");
 
 //target : lo que sea a lo que se le aplique add event listener
 if (typeof input.addEventListener != "undefined") {
-    input.addEventListener(
-        "keyup",
-        evt => {
-            let term = evt.target.value.toLowerCase();
+  input.addEventListener(
+    "keyup",
+    evt => {
+      let term = evt.target.value.toLowerCase();
 
-            let filteredChampions = Object.values(championList).filter(champion => {
-                //indexof da -1 si no encuentra resultados
-                if (champion.name.toLowerCase().indexOf(term) != -1) {
-                    return champion;
-                }
-            });
+      let filteredChampions = Object.values(championList).filter(champion => {
+        //indexof da -1 si no encuentra resultados
+        if (champion.name.toLowerCase().indexOf(term) != -1) {
+          return champion;
+        }
+      });
 
-            //mensaje de error en la busqueda por nombre
-            if (term.length != 0) {
-                list.classList.add("hidden");
-                filteredList.classList.remove("hidden");
-            } else {
-                list.classList.remove("hidden");
-                filteredList.classList.add("hidden");
-            }
-            let errorMessage = document.querySelector("#error");
-            if (filteredChampions.length === 0) {
-                errorMessage.classList.remove("hidden");
-            } else {
-                errorMessage.classList.add("hidden");
-            }
+      //mensaje de error en la busqueda por nombre
+      if (term.length != 0) {
+        list.classList.add("hidden");
+        filteredList.classList.remove("hidden");
+      } else {
+        list.classList.remove("hidden");
+        filteredList.classList.add("hidden");
+      }
+      let errorMessage = document.querySelector("#error");
+      if (filteredChampions.length === 0) {
+        errorMessage.classList.remove("hidden");
+      } else {
+        errorMessage.classList.add("hidden");
+      }
 
-            //vaciar arreglo para que no se duplique
-            filteredList.innerHTML = "";
-            //filteredList ==  <div id="filteredList" ></div>
-            Object.values(filteredChampions).map(champion => {
-                const div = document.createElement("div");
-                const img = document.createElement("img");
-                const p = document.createElement("p");
-                p.className = "championName";
-                img.className = "championSplash";
-                img.src = `${champion.splash}`;
-                p.innerHTML = `${champion.name}`;
-                div.appendChild(img);
-                div.appendChild(p);
-                filteredList.appendChild(div);
-            });
-        },
-        false
-    );
+      //vaciar arreglo para que no se duplique
+      filteredList.innerHTML = "";
+      //filteredList ==  <div id="filteredList" ></div>
+      Object.values(filteredChampions).map(champion => {
+        const div = document.createElement("div");
+        const img = document.createElement("img");
+        const p = document.createElement("p");
+        p.className = "championName";
+        img.className = "championSplash";
+        img.src = `${champion.splash}`;
+        p.innerHTML = `${champion.name}`;
+        div.appendChild(img);
+        div.appendChild(p);
+        filteredList.appendChild(div);
+      });
+    },
+    false
+  );
 }
 
 //[p.btn, p.btn, p.btn, p.btn, p.btn, p.btn, p.btn]
@@ -94,7 +94,6 @@ let buttons = document.querySelectorAll(".btn");
 
 buttons.forEach(button => {
   button.addEventListener("click", evt => {
-
     let term = button.getAttribute("data-value");
     // <p class="btn" data-value="ALL">ALL</p>
     if (term === "ALL") {
