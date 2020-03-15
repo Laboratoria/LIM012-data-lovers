e6df3c2e42c1567ea3c490da1882cca37aaef0bd
Michelle Pineda <michelle.pineda.rivas@gmail.com>
2020-03-14 19:19:28 -0500

    :lipstick: changed dropdown and input color
    changed dropdown and input background color

--- src/main.js
+++ src/main.js
@@ -1,6 +1,4 @@
 import { example } from './data.js';
-// import data from './data/atletas/atletas.js';
-// import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
 
 console.log(example, data);
