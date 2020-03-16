55e02de32cc1524b425ffd5aa3c8f5c93e620ca0
Rossevv <ros22ch.caceres@gmail.com>
2020-03-16 13:16:02 -0500

    UI al 80%

--- src/data.js
+++ src/data.js
@@ -1,4 +1,20 @@
 // estas funciones son de ejemplo
+//entiendo que se trabaja con src/data y se llevan a main.js
+/*Ser puras e independientes del DOM. Funciones que serán usadas desde src/main.js, cada vez que el usuario interactúe (click, filtrado, ordenado, ...).
+ */
+
+/* toda la funcionalidad que corresponda a obtener, procesar y manipular datos (tus funciones). */
+/*filterData(data, condition): esta función filter o filtrar recibiría la data, y nos retornaría aquellos datos que sí cumplan con la condición.
+
+sortData(data, sortBy, sortOrder): esta función sort u ordenar recibe tres parámetros. El primer parámetro, data, nos entrega los datos. 
+El segundo parámetro, sortBy, nos dice con respecto a cuál de los campos de la data se quiere ordenar. El tercer parámetro, sortOrder, indica si se quiere ordenar de manera ascendente o descendente.
+
+computeStats(data): la función compute o calcular, nos permitirá hacer cálculos estadísticos básicos para ser mostrados de acuerdo a la data proporcionada. */
+
+const allSelection = () => {
+
+
+}
 
 export const example = () => 'example';
 
--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
-import { example } from './data.js';
+/*interacción con el DOM event listeners o event handlers, se usan las que estàn en data.js */
+import {
+  example
+} from './data.js';
 // import data from './data/atletas/atletas.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+import data from './data/lol/lol.js';
+// import data from './data/pokemon/pokemon.js';
+
+
+
 
 console.log(example, data);
