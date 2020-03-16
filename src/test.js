a7767f915d0ad97515007aa0d66d0ea43368c2b9
yud-cumba <yudith.cumba@gmail.com>
2020-03-16 10:08:54 -0500

    test inicio

---
+++ src/test.js
@@ -0,0 +1,7 @@
+import { dinamicSearchPokemon } from './data.js';
+
+describe('dinamicSearchPokemon', () => {
+  it('dinamicSearchPokemon debería ser una función', () => {
+    expect(typeof dinamicSearchPokemon).toBe('function');
+  })
+})
