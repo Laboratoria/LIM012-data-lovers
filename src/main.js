c9cc794e18178f4b2afa7d2116a0b541deaf2d92
CarmenVictoriaFarinez <acuario1140@gmail.com>
2020-03-16 14:25:33 -0500

    headerimg

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import { example } from './data.js';
-// import data from './data/atletas/atletas.js';
+//import { example } from './data.js';
+import data from './data/atletas/atletas.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+//import data from './data/pokemon/pokemon.js';
 
 console.log(example, data);
