6b63a1288d5a6feb5127d01f22436f92b0fca320
Sonia Seuc <60791273+soniaseuc@users.noreply.github.com>
2020-03-17 12:34:27 -0500

    martes 17 ordenar ZA consola

--- src/main.js
+++ src/main.js
@@ -21,6 +21,60 @@ Object.values(allChampion).map(champion => {
   img.src = `${champion.splash}`
   p.innerHTML = `${champion.name}`         
   let all = document.getElementById('all');
-  all.appendChild(newElement); 
-  
+  all.appendChild(newElement);   
 });
+
+
+const btnZA = document.getElementById('btnZA');
+btnZA.addEventListener('click', () => {
+document.getElementById("all").style.visibility = 'hidden';
+const newArray = document.createElement('div'); 
+const img = document.createElement('img');
+img.classList.add('class-img');
+const p = document.createElement('p');
+p.classList.add('class-name');
+newArray.appendChild(img);
+newArray.appendChild(p);
+const result = Object.keys(allChampion).sort().reverse();
+result.forEach(agregarElementos);
+function agregarElementos(item, index){         
+  document.getElementById("array").innerHTML += index + ":" + item + "<br>"; 
+}
+  let array = document.getElementById('array');
+array.appendChild(newArray); 
+});
+/*
+const result = Object.keys(allChampion).sort().reverse();
+result.forEach(agregarElementos);
+function agregarElementos(item, index){
+  document.getElementById("array").innerHTML += index + ":" + item + "<br>"; 
+}
+console.log('Arreglo', result);
+});
+
+
+img.src = `${champion.splash}`
+p.innerHTML = `${champion.name}`
+*/
+
+/*const result = Object.keys(allChampion).sort().reverse();
+//document.getElementById("div").innerHTML = Object.keys(allChampion).sort().reverse();;
+console.log('Arreglo', result);
+
+result.forEach(agregarElementos);
+function agregarElementos(item, index){
+  document.getElementById("array").innerHTML += index + ":" + item + "<br>"; 
+}
+
+
+
+  var lista=document.getElementById("all"); 
+  array.forEach(function(data,index){
+  var linew= document.createElement("div");    
+  var contenido = document.createTextNode(data.name);
+  lista.appendChild(linew);
+  linew.appendChild(contenido);
+  
+  })
+  }
+  agregarElementos(result);*/
