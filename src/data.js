3f9e6116ba2bf1b8ca79826a269a1ab8f1cdd7a7
claudiacortezb <claudia.cboc@gmail.com>
2020-03-17 09:38:34 -0500

    Segundo avance

--- src/data.js
+++ src/data.js
@@ -1,10 +1,43 @@
 //Funcionalidad
+import { inputSearchElem } from './main.js';
 
-export const example = () => {
-  console.log('Hola console');
-  return 'Soy el return de example';
-};
 
-// export const search=()=>{
-  
-// }
+//muestro la info
+export const showInfo=(pokes)=>{
+  const inputValue=inputSearchElem.value;
+  let info='';
+  console.log(`Lei input ${inputValue}`)
+      pokes.forEach((poke)=>{
+          if(inputValue==poke.name){
+              info= `
+              <section id="s2img">
+                  <span>${poke.num}</span>
+                  <img src="${poke.img}" alt="pokemonImage">
+                  <span id="namePok"> ${poke.name}</span>
+              </section> 
+              <section id="s2about">
+                  <p>About:<span>${poke.about}</p>
+              </section>
+              <section id="s2info">
+                  <p>Type: <span>${poke.type}</span></p>
+                  <p>Height: <span>${poke.size.height}</span></p>
+                  <p>Weight: <span>${poke.size.weight}</span></p>      
+              </section>
+              <section id="weakRes">
+                  <p>Weakness: <span>${poke.weaknesses}</span></p>
+                  <p>Resistance: <span>${poke.resistant}</span></p>
+              </section>
+              `
+          }
+      });
+      if(info){
+          return info;
+      }
+      else{
+          return 'El nombre ingresado no es correcto';
+      }   
+}
+
+
+//<p>Special Attack: <span>${poke.special-attack}</span></p> 
+
