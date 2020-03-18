cdb1712df3963b3822963a4ded1768d991787edf
Rossevv <ros22ch.caceres@gmail.com>
2020-03-17 20:00:46 -0500

    cambios recuperados

--- src/main.js
+++ src/main.js
@@ -3,10 +3,39 @@ import {
   example
 } from './data.js';
 // import data from './data/atletas/atletas.js';
-import data from './data/lol/lol.js';
+import lol from './data/lol/lol.js';
 // import data from './data/pokemon/pokemon.js';
 
+const allchampionList = lol.data;
+console.log(allchampionList);
+let list = document.querySelector("#root");
+console.log(root);
+//objects
+Object.values(allchampionList).forEach(champion => {
+  const div = document.createElement("div");
+  const img = document.createElement("img");
+  const p = document.createElement("p");
+  p.className = "nameOfChampion";
+  img.className = "imageOfChampion";
+  p.innerHTML = `${champion.name}`;
+  img.src = `${champion.splash}`;
+  div.appendChild(img);
+  div.appendChild(p);
+  list.appendChild(div);
+});
 
 
 
-console.log(example, data);
+
+
+
+
+
+
+
+// let rol = lol.data;
+// let newArrays = Object.entries(rol);
+// let onlyFighter = newArrays.filter()
+
+
+// console.log(example, data);
