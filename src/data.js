dc30e96edc17ec707657e54251ba411ddca131b3
CarmenVictoriaFarinez <acuario1140@gmail.com>
2020-03-19 11:27:48 -0500

    primerafuncion

--- src/data.js
+++ src/data.js
@@ -1,5 +1,15 @@
 // estas funciones son de ejemplo
 
-export const example = () => 'example';
+export const newArray = atletas.map((atletas) => {
+
+    return {
+      nombre : atletas.nombre,
+      deporte: atletas.deporte,
+      equipo: atletas.noc
+    }
+    
+  })
+  console.log(newArray)
+  document.getElementById("contenedor").innerHTML = newArray;
 
 export const anotherExample = () => 'OMG';
