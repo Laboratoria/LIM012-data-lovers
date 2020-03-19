285cbe9254fe3af4cb26572f9949d46b725d43a6
claudiacortezb <claudia.cboc@gmail.com>
2020-03-19 17:18:20 -0500

    no lo haga compa

--- src/data.js
+++ src/data.js
@@ -1,39 +1,9 @@
-//Funcionalidad
-import { inputSearchElem } from './main.js';
+// estas funciones son de ejemplo
 
-//muestro la info
-export const showInfo=(pokes)=>{
-  const inputValue=inputSearchElem.value;
-  let info='';
-  console.log(`Lei input ${inputValue}`)
-      pokes.forEach((poke)=>{
-          if(inputValue==poke.name){
-              info= `
-              <section id="s2img">
-                  <span>${poke.num}</span>
-                  <img src="${poke.img}" alt="pokemonImage">
-                  <span id="namePok"> ${poke.name}</span>
-              </section> 
-              <section id="s2about">
-                  <p>About:<span>${poke.about}</p>
-              </section>
-              <section id="s2info">
-                  <p>Type: <span>${poke.type}</span></p>
-                  <p>Height: <span>${poke.size.height}</span></p>
-                  <p>Weight: <span>${poke.size.weight}</span></p>      
-              </section>
-              <section id="weakRes">
-                  <p>Weakness: <span>${poke.weaknesses}</span></p>
-                  <p>Resistance: <span>${poke.resistant}</span></p>
-              </section>
-              `
-          }
-      });
-      if(info){
-          return info;
-      }
-      else{
-          return 'El nombre ingresado no es correcto';
-      }   
-}
+export const example = () => {
+  return 'example';
+};
 
+export const anotherExample = () => {
+  return 'OMG';
+};
--- src/main.js
+++ src/main.js
@@ -1,22 +1,10 @@
-
+import { example } from './data.js';
+// import data from './data/injuries/injuries.js';
+// import data from './data/lol/lol.js';
+// import data from './data/patient/patient.js';
 import data from './data/pokemon/pokemon.js';
-import {showInfo} from './data.js';
+// import data from './data/rickandmorty/rickandmorty.js';
+// import data from './data/steam/steam.js';
+// import data from './data/worldbank/worldbank.js';
 
-const pokemones=data.pokemon;
-
-export const inputSearchElem=document.getElementById("inputSearch");
-const searchIconElem=document.getElementById("searchIcon");
-const screen1Elem=document.getElementById('screen1');
-const screen2Elem=document.getElementById('screen2');
-
-
-
-const showScreen2=()=>{
-    screen1Elem.style.display="none";
-    screen2Elem.style.display="block";
-
-    screen2Elem.innerHTML=` Hay ${pokemones.length} pokemones
-    ${showInfo(pokemones)}`
-
-}
-searchIconElem.addEventListener('click',showScreen2); 
+console.log(example, data);
