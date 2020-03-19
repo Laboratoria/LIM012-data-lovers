8c1e5dff48d7e03a8435d6aac6eb1d2e1f1de91d
Rossevv <ros22ch.caceres@gmail.com>
2020-03-19 08:53:41 -0500

    Funcion filtrar

--- src/data.js
+++ src/data.js
@@ -5,30 +5,23 @@
 
 /* toda la funcionalidad que corresponda a obtener, procesar y manipular datos (tus funciones). */
 /*filterData(data, condition): esta función filter o filtrar recibiría la data, y nos retornaría aquellos datos que sí cumplan con la condición.
-
 sortData(data, sortBy, sortOrder): esta función sort u ordenar recibe tres parámetros. El primer parámetro, data, nos entrega los datos. 
 El segundo parámetro, sortBy, nos dice con respecto a cuál de los campos de la data se quiere ordenar. El tercer parámetro, sortOrder, indica si se quiere ordenar de manera ascendente o descendente.
-
 computeStats(data): la función compute o calcular, nos permitirá hacer cálculos estadísticos básicos para ser mostrados de acuerdo a la data proporcionada. */
 
-const allSelection = (allchampionList) => {
-    let arrayKeysChampions = Object.keys(allchampionList);
-    let arrayValuesChampions = Object.values(allchampionList);
-    // console.log(prueba2);
-    for (let i = 0; i < arrayKeysChampions.length; i += 1) {
-      // console.log(prueba3[i][1]);
-      // console.log(prueba2[i].tags);
-      let roleChampions = arrayValuesChampions[i].tags;
-      // console.log(roleChampions);
-      for (let j = 0; j < roleChampions.length; j += 1) {
-        // console.log(roleChampions[j]) /*aparecen todos los roles */
-        if (roleChampions[j] === "Fighter") {
+export const allSelection = (allchampionList, typeRol) => {
+  let arrayValuesChampions = Object.values(allchampionList);
+  arrayValuesChampions.filter((championsList) => {
+    if (championsList.tags.indexOf(typeRol) != -1) {
+      return championsList;
+      // return {
+      //   name: (championsList.name),
+      //   splash: (championsList.splash),
+      //   tags: (championsList.tags)
+    }
+  })
+  return arrayValuesChampions;
+}
+export const example = () => 'example';
 
-          // console.log(arrayValuesChampions[i].name);
-          // console.log(arrayValuesChampions[i].splash);
-
-        }
-
-        // export const example = () => 'example';
-
-        export const anotherExample = () => 'OMG';
+export const anotherExample = () => 'OMG';
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 /*interacción con el DOM event listeners o event handlers, se usan las que estàn en data.js */
 import {
-  example
+  allSelection
 } from './data.js';
 // import data from './data/atletas/atletas.js';
 import lol from './data/lol/lol.js';
@@ -25,44 +25,40 @@ Object.values(allchampionList).forEach(champion => {
 });
 
 
-
 let listRole = document.querySelector("#rolesFighter");
 
-// let rol = lol.data;
+let liRoles = document.querySelectorAll('.Roles');
+// console.log(liRoles);
+// console.log(typeof liRoles);
 
+liRoles.forEach((option) => {
+  let typeRol = option.getAttribute('data-value')
+  console.log(typeRol);
 
-let arrayKeysChampions = Object.keys(allchampionList);
-let arrayValuesChampions = Object.values(allchampionList);
-// console.log(prueba2);
-let prueba3 = Object.entries(allchampionList); /*solo para hacer las pruebas */
+  const functionFilterRol = allSelection(allchampionList, typeRol)
+  console.log(functionFilterRol);
+  console.log(typeof functionFilterRol);
 
-for (let i = 0; i < arrayKeysChampions.length; i++) {
-  // console.log(prueba3[i][1]);
-  // console.log(prueba2[i].tags);
-  let roleChampions = arrayValuesChampions[i].tags;
-  // console.log(roleChampions);
-  for (let j = 0; j < roleChampions.length; j++) {
-    // console.log(roleChampions[j]) /*aparecen todos los roles */
-
-    if (roleChampions[j] === "Fighter") {
-
-      const divRole = document.createElement("div");
-      const imgRole = document.createElement("img");
-      const pName = document.createElement("p");
-      const pTags = document.createElement("p");
-      pName.className = "nameOfChampionRole";
-      imgRole.className = "imageOfChampionRole";
-      pTags.className = "roleTagsOfChampions";
-      pName.innerHTML = arrayValuesChampions[i].name;
-      imgRole.src = arrayValuesChampions[i].splash;
-      pTags.innerHTML = arrayValuesChampions[i].tags;
-      divRole.appendChild(imgRole);
-      divRole.appendChild(pName);
-      divRole.appendChild(pTags);
-      listRole.appendChild(divRole);
-      // console.log(arrayValuesChampions[i].name);
-      // console.log(arrayValuesChampions[i].splash);
-    }
-  }
-}
-/*con mayúsucla Object */
+  // /*con mayúsucla Object */
+  // let arrayValuesChampions = Object.values(allchampionList);
+  // arrayValuesChampions.filter((championsList) => {
+  //   if (championsList.tags.indexOf("Fighter") != -1) {
+  const divRole = document.createElement("div");
+  const imgRole = document.createElement("img");
+  const pName = document.createElement("p");
+  const pTags = document.createElement("p");
+  pName.className = "nameOfChampionRole";
+  imgRole.className = "imageOfChampionRole";
+  pTags.className = "roleTagsOfChampions";
+  pName.innerHTML = functionFilterRol.name;
+  imgRole.src = functionFilterRol.splash;
+  pTags.innerHTML = functionFilterRol.tags;
+  divRole.appendChild(imgRole);
+  divRole.appendChild(pName);
+  divRole.appendChild(pTags);
+  listRole.appendChild(divRole);
+  // console.log(arrayValuesChampions[i].name);
+  // console.log(arrayValuesChampions[i].splash);
+  //   }
+  // })
+})
