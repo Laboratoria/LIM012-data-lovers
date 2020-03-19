a2ffc13678ae51483aa6a7f365bae34bb5c1b7f9
Sandy Marmolejo <sandymarmolejoc@gmail.com>
2020-03-19 16:27:02 -0500

    Estructura de html

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { example } from './data.js';
-// import data from './data/atletas/atletas.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+import data from './data/atletas/atletas.js';
+
 
 console.log(example, data);
