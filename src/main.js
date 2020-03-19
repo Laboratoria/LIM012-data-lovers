4a939b58ca4e0ec57a76353f6e4d8e3a1f102ed5
Lourdes0896 <sam.lourdesmerino@gmail.com>
2020-03-19 09:00:22 -0500

    historia 1

--- src/main.js
+++ src/main.js
@@ -1,6 +1,59 @@
 import { example } from './data.js';
 
 import lol from './data/lol/lol.js';
+
+const listaTodos=lol.data; //en esta variable guardo los objetos de la data
+//console.log(listaTodos);
+let lista = document.querySelector("#galeria"); //selecciono la parte donde voy a poner la informacion
+//que seleccionaré y lo guardo en una variable
+//console.log(lista);
+   
+Object.values(listaTodos).map(campeones=> {
+    const casilla = document.createElement('div');
+    const foto = document.createElement('img');
+    const nombre = document.createElement('p');
+    nombre.className = 'nombreCampeon'; //le pongo un class nombre para editarle los estilos
+    foto.className = 'imagenCampeon';
+    nombre.innerHTML = `${campeones.name}`; 
+    foto.src = `${campeones.splash}`;
+    
+    casilla.appendChild(foto);
+    casilla.appendChild(nombre);
+    lista.appendChild(casilla);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*const listaCampeones=lol.data;
+console.log(listaCampeones);
+
 const imagen = lol.data.Aatrox.splash;
 const foto=document.getElementById("foto");
 foto.src=imagen;
@@ -14,7 +67,11 @@ console.log(mostrarNombres);
 
 
 const mostrarImagen= Object.values(lol.data.src.splash);
-console.log(mostrarImagen);
+console.log(mostrarImagen);*/
+
+
+
+
 /*const mostrarTodos= " ";
 for (mostrarTodos in lol.data){
     console.log(lol.data[mostrarTodos]);
