fc565ca95d3a3b5ac596645d5ae558c84315ffed
Lemi-Torres <lmt.2020v@gmail.com>
2020-03-20 14:38:04 -0500

    nuevo

--- src/data.js
+++ src/data.js
@@ -1,16 +1,8 @@
+
 //AQUI MANEJAMOS LAS FUNCIONES
 
 // estas funciones son de ejemplo
 //export const example = () => 'example';  
 //export const anotherExample = () => 'OMG';
 
-/*export const crearTemplate = (arr) =>{
-    let stringTemplate ='';
-    for (let i = 0; 1 > arr.length; i++) {
-        stringTemplate += `<div>
-        <p>${arr[i][i].id}</p>
-        <img src = ${arr[i][i].splash}>
-        </div>`
-    }
-    return stringTemplate
-}*/
+
--- test/data.spec.js
+++ test/data.spec.js
@@ -1,21 +1,6 @@
 //import { example, anotherExample } from '../src/data.js';
-import { crearTemplate } from '../src/data.js';
 
-import data from "../src/data/lol/lol.js";
-let allChampion = data.data;
-
-describe('crearTemplate', () => {
-  it('is a function', () => {
-    expect(typeof crearTemplate).toBe('function');
-  });
-
-  it('returns Campeon Aatrox', () => {
-    expect(crearTemplate(allChampion,'Aatrox')).toBe([allChampion.Aatrox]);
-  });
-});
-
-
-describe('anotherExample', () => {
+/*describe('anotherExample', () => {
   it('is a function', () => {
     expect(typeof anotherExample).toBe('function');
   });
@@ -23,4 +8,4 @@ describe('anotherExample', () => {
   it('returns `anotherExample`', () => {
     expect(anotherExample()).toBe('OMG');
   });
-});
+});*/
