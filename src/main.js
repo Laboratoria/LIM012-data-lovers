00d755548bc7008d0c39e305046d69928004322d
katpimo <kat.pimo@gmail.com>
2020-03-21 02:03:52 -0500

    avanzando css

--- src/main.js
+++ src/main.js
@@ -1,24 +1,22 @@
-//import { example } from './data.js';
+// import { example } from './data.js';
 import lol from './data/lol/lol.js';
 
-const listaTodos=lol.data; 
-//console.log(listaTodos);
-let lista = document.querySelector("#galeria"); 
-//console.log(lista);
-   
-Object.values(listaTodos).map(campeones=> {
-    const casilla = document.createElement('div');
-    const foto = document.createElement('img');
-    const nombre = document.createElement('p');
-    nombre.className = 'nombreCampeon'; 
-    foto.className = 'imagenCampeon';
-    nombre.innerHTML = `${campeones.name}`; 
-    foto.src = `${campeones.splash}`;
-    
-    casilla.appendChild(foto);
-    casilla.appendChild(nombre);
-    lista.appendChild(casilla);
+const listaTodos = lol.data;
+// console.log(listaTodos);
+const lista = document.querySelector('#galeria');
+// console.log(lista);
+
+Object.values(listaTodos).forEach((campeones) => {
+  const casilla = document.createElement('div');
+  const foto = document.createElement('img');
+  const nombre = document.createElement('p');
+  casilla.className = 'casillaCampeon';
+  nombre.className = 'nombreCampeon';
+  foto.className = 'imagenCampeon';
+  nombre.innerHTML = `${campeones.name}`;
+  foto.src = `${campeones.splash}`;
+
+  casilla.appendChild(foto);
+  casilla.appendChild(nombre);
+  lista.appendChild(casilla);
 });
-
-
-
