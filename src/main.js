e131d758f5a4fde738bd00eaee56261bff160c34
IsabelaSanchez <natyst_26@hotmail.com>
2020-03-23 20:07:22 -0500

    prueba 3

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import { example } from './data.js';
 
 //CONSOLE LOGS DE PRUEBA
 //esta es la rama de Isabela
+//esta es nuestra rama
 let stringTemplate = '';
 const container=document.getElementById('container');
 
