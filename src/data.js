574dee2fef99c9bf0706275a8965f4ece4b09cf1
Sandy Marmolejo <sandymarmolejoc@gmail.com>
2020-03-24 17:14:27 -0500

    Modificacion de estilos y html

--- src/data.js
+++ src/data.js
@@ -1,17 +1,16 @@
 
-export const mostrarAtletasConSuDeporte = (data) => {
-  // eslint-disable-next-line arrow-body-style
-  const atletasConDeporte = data.atletas.map((a) => {
+export const athletesView = (data) => {
+// eslint-disable-next-line arrow-body-style
+  const paintAthletes = data.atletas.map((a) => {
     return {
       nombre: a.nombre,
       deporte: a.deporte,
-      equipo: a.noc,
+      equipo: a.equipo,
     };
   });
-
-  return atletasConDeporte;
+  return paintAthletes;
 };
-
-
 export const example = () => 'example';
 export const anotherExample = () => 'OMG';
+ 
+
--- src/main.js
+++ src/main.js
@@ -1,30 +1,37 @@
-/* eslint-disable */
-import { example, athletesView } from './data.js';
+/* eslint-disable spaced-comment */
+// eslint-disable-next-line import/named
+import { athletesView } from './data.js';
 import data from './data/atletas/atletas.js';
  
-const opcionesMenu = document.querySelectorAll('li');
-opcionesMenu.forEach(element=> {
+const optionsMenu = document.querySelectorAll('li');
+optionsMenu.forEach((element) => {
   element.addEventListener('click', (event) => {
+    debugger;
+    document.getElementById('table').classList.remove('borrar');
     const paintAthletes = athletesView(data);
     if (event.target.id === '1') {
       document.getElementById('root').innerHTML = '';
-      paintAthletes.forEach(element => {
+      document.getElementById('articulos').classList.add('borrar');
+
+      // eslint-disable-next-line no-shadow
+      paintAthletes.forEach((paintAthletes) => {
         const ulAtleta = document.createElement('ul');
         const liNombre = document.createElement('li');
-        const textNombre = document.createTextNode(element.nombre);
+        const textNombre = document.createTextNode(paintAthletes.nombre);
         liNombre.appendChild(textNombre);
         ulAtleta.appendChild(liNombre);
         const liEquipo = document.createElement('li');
-        const textEquipo = document.createTextNode(element.equipo);
+        const textEquipo = document.createTextNode(paintAthletes.equipo);
         liEquipo.appendChild(textEquipo);
         ulAtleta.appendChild(liEquipo);
         const liDeporte = document.createElement('li');
-        const textDeporte = document.createTextNode(element.deporte);
+        const textDeporte = document.createTextNode(paintAthletes.deporte);
         liDeporte.appendChild(textDeporte);
         ulAtleta.appendChild(liDeporte);
         document.getElementById('root').appendChild(ulAtleta);
       });
-    }
-  })
+
+   }
+  });
 });
-console.log(example, data);
+
