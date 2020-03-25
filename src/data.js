f4a1d06ca44aed484b997693bfcc4bd094d2a789
claudiacortezb <claudia.cboc@gmail.com>
2020-03-25 10:06:59 -0500

    actualizacion2

--- src/data.js
+++ src/data.js
@@ -1,73 +1,103 @@
-//Funcionalidad en data
-import { inputSearchElem } from './main.js';
-//muestro la info
-
-export const showInfo=(pokes)=>{
-  const inputValue=inputSearchElem.value;
-  let info='';
-//   console.log(`Lei input ${inputValue}`)
-      pokes.forEach((poke)=>{
-         if(inputValue==poke.name){
-             info= `
+//  Funcionalidad en data
+//  muestro la info
+export const showInfo = (pokes,input) => {
+  const inputVal = input.value;
+  let info = '';
+  pokes.forEach((poke) => {
+    if (inputVal === poke.name) {
+      info = `
               <p class="namePok"> ${poke.name}</p>
-              <section id="imgCont">
-                  <span class="circle">${poke.num}</span>
-                  <img src="${poke.img}" alt="pokemonImage">
-              </section> 
-              <section id="s2about">
-                    <p id="about" class="infoCont"> <span>${poke.about}</span></p>
-                    <div id="s2info" class="infoCont data">
-                        <p>Type:<span>${poke.type}</span></p>
-                        <p>Height:<span>${poke.size.height}</span></p>
-                        <p>Weight:<span>${poke.size.weight}</span></p>      
+              <section id=firstRow>
+                <div id="imgCont">
+                    <span class="circle ft">${poke.num}</span>
+                    <img src="${poke.img}" alt="pokemonImage">
+                </div>
+                <div id="s2info" class="infoCont">
+                          <p>Type:<span>${poke.type}</span></p>
+                          <p>Weakness:<span>${poke.weaknesses}</span></p>
+                          <p>Resistance:<br><span>${poke.resistant}</span></p>
+                </div>
+              </section>
+                    <div id="hW" class="infoCont">
+                      <p>Height:<span>${poke.size.height}</span></p>
+                      <p>Weight:<span>${poke.size.weight}</span></p>  
+                      <p>Base Attack:<span>${poke.stats['base-attack']}</span></p>
                     </div>
-                    <div id="weakRes" class="infoCont data">
-                        <p>Weakness:<span>${poke.weaknesses}</span></p>
-                        <p>Resistance:<span>${poke.resistant}</span></p>
-                    </div>
+                    <p id="about" class="infoCont">${poke.about}</p>
                </section>
-              `
-         }
-      });
-      if(info){
-          return info;
-      }
-      else{
-          return 'El nombre ingresado no es correcto';
-      }   
-}
-export const showAllData=(data)=>{
-    return `
-        <div class="s3">
+              `;
+    }
+  });
+  if (info) {
+    return info;
+  }
+  return 'El nombre ingresado no es correcto';
+};
+export const showAllData = (data) => {
+  return `
         <div id="imgCont" class="item">
-            <span class="circle">${data.num}</span>
+            <span class="circle ft">${data.num}</span>
             <img src="${data.img}" alt="pokemonImage">
             <span> ${data.name}</span>
-        </div> 
-        <span class="lor">nel</span>
-        <div class="lor">gel</div>
-        </div>   
-        `
-}
-/* export const orderAZ=(data)=>{
-    return `
-        <div class="s3">
-            <div id="imgCont" class="item">
-                <span class="circle">${data.num}</span>
-                <img src="${data.img}" alt="pokemonImage">
-                <span> ${data.name}</span>
-            </div> 
         </div>
-        `
-        
-
-}
- */
-
-export const orderAZ=(poke)=>{
-    let ordered=[];
-    for(let i=0;i<poke.length;i++){
-        ordered[i]+=poke[i].name;
+        `;
+};
+// Ordena Alfabeticamente
+export const orderBy = (poke, order) => {
+  let arrSort = [];
+  arrSort = poke.sort((a, b) => {
+    const nameA = a.name;
+    const nameB = b.name;
+    const numA = a.num;
+    const numB = b.num;
+    if (order === 'asc') {
+      if (nameA > nameB) {
+        return 1;
+      }
+      else {
+        if (nameA < nameB) {
+          return -1;
+        }
+        {
+          return 0;
+        }
+      }
+      /* return (nameA > nameB) ? 1 : ((nameA < nameB) ? -1 : 0); */
     }
-    console.log(ordered);
-}
+    if (order === 'desc') {
+      if (nameA > nameB) {
+        return -1;
+      }
+      else {
+        if (nameA < nameB) {
+          return 1;
+        }
+        {
+          return 0;
+        }
+      }
+    }
+    //  numero descendente
+
+    if (order === 'numUp') {
+      if (numA > numB) {
+        return -1;
+      }
+      else {
+        if (numA < numB) {
+        return 1;
+        }
+        {
+         return 0;
+        }
+      }
+    }
+  });
+  return arrSort;
+};
+// Filtra por tipo
+export const typeFilter = (poke, tipo) => {
+  // eslint-disable-next-line no-shadow
+  const arrFilt = poke.filter(poke => poke.type.includes(tipo)); // retorna un boolean
+  return arrFilt;
+};
