d4e5fb2c575fd263cde2ed4f58b2ebef4045609b
Lourdes0896 <sam.lourdesmerino@gmail.com>
2020-03-26 08:52:14 -0500

    cambios H

--- src/data.js
+++ src/data.js
@@ -1,8 +1,28 @@
 // estas funciones son de ejemplo
 
-export const example = () => 'example';
+export const ordenarCampeones = (listaTodos, texto) => {
+  let ordenarLista;
+
+  if (texto === 'az') {
+    ordenarLista = Object.values(listaTodos).sort();
+  }
+  if (texto === 'za') {
+    ordenarLista = Object.values(listaTodos)
+      .sort()
+      .reverse();
+  }
+  return ordenarLista;
+};
 
 
+export const filtroNombre = (data, texto) => {
+  const longitudcadenaBuscada = texto.length;
+  const filtroCampeones = Object.values(data).filter((campeones) => {
+    if (texto === campeones.name.toLowerCase().substring(0, longitudcadenaBuscada)) {
+      return campeones; // retorna el objeto encontrado
+    }
+  });
 
-
+  return filtroCampeones; // retornamos el array de objetos encontrados
+};
 export const anotherExample = () => 'OMG';
--- src/main.js
+++ src/main.js
@@ -1,64 +1,61 @@
 import lol from './data/lol/lol.js';
+import { filtroNombre, ordenarCampeones } from './data.js';
 
 const listaTodos = lol.data; // en esta variable guardo los objetos de la data
-// console.log(listaTodos);
 const lista = document.querySelector('#galeria'); // selecciono la parte donde voy a poner la informacion
-// que seleccionaré y lo guardo en una variable
-// console.log(lista);
 
-Object.values(listaTodos).forEach((campeones) => {
-  const casilla = document.createElement('div');
-  const foto = document.createElement('img');
-  const nombre = document.createElement('p');
-  casilla.className = 'casillaCampeon';
-  nombre.className = 'nombreCampeon'; // le pongo un class nombre para editarle los estilos
-  foto.className = 'imagenCampeon';
-  nombre.innerHTML = `${campeones.name}`;
-  foto.src = `${campeones.splash}`;
+const resultado = (data) => {
+  // vaciar arreglo para que no se duplique
+  lista.innerHTML = '';
+  const checkData = Array.isArray(data) // chequear la estructura de la data, si no es un array pasa a Object.values
+    ? data
+    : Object.values(data);
+  checkData.forEach((campeones) => {
+    const casilla = document.createElement('div');
+    const foto = document.createElement('img');
+    const nombre = document.createElement('p');
+    nombre.className = 'nombreCampeon'; // le pongo un class nombre para editarle los estilos
+    foto.className = 'imagenCampeon';
+    nombre.innerHTML = `${campeones.name}`;
+    foto.src = `${campeones.splash}`;
+    casilla.className = 'casillaCampeon';
+    casilla.appendChild(foto);
+    casilla.appendChild(nombre);
+    lista.appendChild(casilla);
+  });
+};
+resultado(listaTodos);
 
-  casilla.appendChild(foto);
-  casilla.appendChild(nombre);
-  lista.appendChild(casilla);
+// buscando en el input
+const buscar = document.querySelector('#buscador');
+buscar.addEventListener(
+  'keyup',
+  (evt) => {
+    const texto = evt.target.value.toLowerCase();// extraemos el valor de la caja de texto
+    const filtroCampeon = filtroNombre(listaTodos, texto); // llamo mi funcion
+
+    // CHAMPION NOT FOUND
+    const errorMsj = document.querySelector('#error');
+    if (filtroCampeon.length === 0) {
+      errorMsj.style.display = 'block';
+    } else {
+      errorMsj.style.display = 'none';
+    }
+    resultado(filtroCampeon);
+  },
+  false,
+);
+
+
+// boton de ordenar
+const AZ = document.getElementById('ascendente');
+AZ.addEventListener('click', () => {
+  const dataOrdenada = ordenarCampeones(listaTodos, 'az');
+  resultado(dataOrdenada);
 });
 
-const buscarCampeon = document.getElementById('botonBuscar');
-buscarCampeon.addEventListener('click', (Busqueda) => {
-  Busqueda.preventDefault();
-  const campeonBuscado = document.getElementById('buscador').value;
-  console.log(campeonBuscado);
-  const campeonEncontrado = Object.values(listaTodos).filter((campeones) => (campeones.name === campeonBuscado));
-  console.log(campeonEncontrado);
+const ZA = document.getElementById('descendente');
+ZA.addEventListener('click', () => {
+  const dataOrdenada = ordenarCampeones(listaTodos, 'za');
+  resultado(dataOrdenada);
 });
-
-
-/* const listaCampeones=lol.data;
-console.log(listaCampeones);
-
-const imagen = lol.data.Aatrox.splash;
-const foto=document.getElementById("foto");
-foto.src=imagen;
-
-const campeon= lol.data.Aatrox.name;
-const galeria=document.getElementById("galeria");
-galeria.innerHTML = campeon;
-
-const mostrarNombres= Object.keys(lol.data);
-console.log(mostrarNombres);
-
-
-const mostrarImagen= Object.values(lol.data.src.splash);
-console.log(mostrarImagen); */
-
-
-/* const mostrarTodos= " ";
-for (mostrarTodos in lol.data){
-    console.log(lol.data[mostrarTodos]);
-} */
-
-
-/* for (campeon in data){
-   console.log (data [campeon];
-} */
-
-
-// console.log(example, data);
