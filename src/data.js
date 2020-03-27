7a381c6cdfc92d37dd00a6489b97208abdd8b60a
Sandy Marmolejo <sandymarmolejoc@gmail.com>
2020-03-27 13:23:40 -0500

    Test de funcion ordenar

--- src/data.js
+++ src/data.js
@@ -1,15 +1,19 @@
-export const athletesView = (data) => {
-  const paintAthletes = data.atletas.map((a) => {
-    return {
-      nombre: a.nombre,
-      deporte: a.deporte,
-      equipo: a.equipo,
-    };
-  });
-  return paintAthletes;
+// Las funciones son acciones y las acciones son verbos!
+// Cual es el nombre de la funcion: el tipo de dato de sus parmetros y lo que retorna
+
+
+export const athletesView = (array) => {
+  const newArray = array.map(obj => ({
+    nombre: obj.nombre,
+    deporte: obj.deporte,
+    equipo: obj.equipo,
+  }));
+  return newArray;
 };
-export const ordenAs = (paintAthletes) => {
-  const ascendente = paintAthletes.sort((prev, next) => {
+
+
+export const ordenAs = (p1) => {
+  const ascendente = p1.sort((prev, next) => {
     if (prev.nombre > next.nombre) {
       return 1;
     } if (prev.nombre < next.nombre) {
