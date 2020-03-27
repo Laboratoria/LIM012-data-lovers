70abc38789dc255ae776af447751ac7384db900e
CarmenVictoriaFarinez <acuario1140@gmail.com>
2020-03-27 14:04:14 -0500

    testsinconflicto

--- src/data.js
+++ src/data.js
@@ -13,9 +13,7 @@ export const ordenAs = (p1) => {
   const ascendente = p1.sort((prev, next) => {
     if (prev.nombre > next.nombre) {
       return 1;
-    } if (prev.nombre < next.nombre) {
-      return -1;
-    } return 0;
+    } return -1;
   });
   return ascendente;
 };
