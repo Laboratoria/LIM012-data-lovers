7cd8f849421e5723ae202c5e004863735c9a301b
Sandy Marmolejo <sandymarmolejoc@gmail.com>
2020-03-27 13:44:16 -0500

    Test de orden alfabeticamente

--- src/data.js
+++ src/data.js
@@ -1,7 +1,6 @@
 // Las funciones son acciones y las acciones son verbos!
 // Cual es el nombre de la funcion: el tipo de dato de sus parmetros y lo que retorna
 
-
 export const athletesView = (array) => {
   const newArray = array.map(obj => ({
     nombre: obj.nombre,
@@ -10,7 +9,6 @@ export const athletesView = (array) => {
   }));
   return newArray;
 };
-
 
 export const ordenAs = (p1) => {
   const ascendente = p1.sort((prev, next) => {
