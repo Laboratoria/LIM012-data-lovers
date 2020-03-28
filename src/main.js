d6324a2da5455f6917f85feedb4a4c1230e0d38e
AlmeAld <almenaldana@gmail.com>
2020-03-28 13:39:50 -0500

    css

--- src/main.js
+++ src/main.js
@@ -9,15 +9,17 @@ console.log(dataAtletas);
 const data = (dataAtletas.atletas);
 
 document.getElementById('main').innerHTML = `
-<h2 class='title' >TODOS LOS ATLETAS</h2>
+<h2 class='titleh2' >TODOS LOS ATLETAS</h2>
 
 <div class="container">
 ${data.map(dataPrincipal => `
 <div class="cards">
+<img src=${dataPrincipal.gender === 'F' ? './img/avatarMujer.jpg' : './img/avatarVaron.jpg'} class="imgAvatar" >
 <h2>${dataPrincipal.name}</h2>
-<p><strong>Sport :</strong> ${dataPrincipal.sport} </p>
-<p><strong>Weigth :</strong> ${dataPrincipal.weigth} </p>
-<p><strong>Team:</strong> ${dataPrincipal.team} </p>
+<p><strong class = "description">Sport :</strong> ${dataPrincipal.sport} </p>
+<p><strong class = "description">Weigth :</strong> ${dataPrincipal.weight} </p>
+<p><strong class = "description">Team:</strong> ${dataPrincipal.team} </p>
+<img src="https://www.countryflags.io/${dataPrincipal.noc}/flat/64.png" class>
 </div>
 `).join('')}
 </div>
