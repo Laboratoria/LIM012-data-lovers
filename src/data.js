cede1d3a1e24030184141a08b30881665a43f40a
Sonia Seuc <60791273+soniaseuc@users.noreply.github.com>
2020-03-27 23:54:03 -0500

    27-03 subiendo a rama master para peer feedback HU1 y 2

--- src/data.js
+++ src/data.js
@@ -4,6 +4,7 @@
 
 // AQUI MANEJAMOS LAS FUNCIONES
 
+// Funcion de ordenar alfabeticamente
 export const orderChampions = (property, orderFrom) => {
   const datesOfChampions = property;
 
@@ -23,5 +24,25 @@ export const orderChampions = (property, orderFrom) => {
       return -1;
     });
   }
+  /* if (orderFrom === 'TODOS') {
+    datesOfChampions.sort((a, b) => {
+      if (a.name > b.name) {
+        return 1;
+      }
+      return -1;
+    });
+  } */
   return datesOfChampions;
 };
+
+// funcion de filtrar incompleta
+let entradasInvalidas = 0;
+export const filtrarPorTags = (obj) => {
+  if (obj.tags.includes('Mage')) {
+    return true;
+  }
+  // eslint-disable-next-line no-plusplus
+  entradasInvalidas++;
+  return false;
+};
+console.log('Número de Entradas Invalidas = ', entradasInvalidas);
--- src/main.js
+++ src/main.js
@@ -13,6 +13,7 @@
 
 import data from './data/lol/lol.js';
 import { orderChampions } from './data.js';
+import { filtrarPorTags } from './data.js';
 
 console.log(data.data);
 
@@ -21,9 +22,11 @@ const arrObjetos = Object.values(allChampion);
 
 //Object.values(allChampion).map((champion) => {
 
+// Funcion para pintar toda la data en el HTML usando appendChild y createElement
 const crearTemplate = (arr) => {
   arr.forEach((champion) => {
     const newElement = document.createElement('div');
+    newElement.classList.add('class-div');
     const img = document.createElement('img');
     img.classList.add('class-img');
     const p = document.createElement('p');
@@ -36,6 +39,9 @@ const crearTemplate = (arr) => {
     box.appendChild(newElement);
   });
 };
+
+/*********************************************** */
+// otra forma de pintar en el html toda la data de campeones
 crearTemplate(arrObjetos);
 
 /*const crearTemplate = (arr) => {
@@ -50,6 +56,8 @@ crearTemplate(arrObjetos);
   return all.join('');
 };*/
 
+/*******************************/
+// Lamando a la funcion ordenar
 const orderAll = document.querySelector('#order');
 
 orderAll.addEventListener('change',() => {
@@ -60,7 +68,38 @@ orderAll.addEventListener('change',() => {
 
 /*document.getElementById('box').innerHTML = crearTemplate(arrObjetos);
 
+// otra forma de llamar a la funcion de ordenar alfabeticamente
 document.querySelector('#order').addEventListener('change', () => {
   const order = document.querySelector('#order').value;
   document.getElementById('box').innerHTML = crearTemplate(orderChampions(arrObjetos, order));
 });*/
+
+// console.log(arrObjetos);
+// console.log(arrObjetos[0].tags);
+
+// utilizando un ejemplo de arreglo de objetos para la funcion de filtrar
+/* const array = [
+  { id: 'Aatrox', tags: ['Fighter', 'Tank'], splash: 'http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Aatrox_0.jpg' },
+  { id: 'Ahri', tags: ['Mage', 'Assassin'], splash: 'http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Ahri_0.jpg' },
+  { id: 'Akali', tags: ['Assassin'], splash: 'http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Akali_0.jpg' },
+  { id: 'Alistar', tags: ['Tank', 'Support'], splash: 'http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Alistar_0.jpg' },
+  { id: 'Zyra', tags: ['Mage', 'Support'], splash: 'http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Zyra_0.jpg' },
+  { id: 'Zed', tags: ['Assassin', 'Fighter'], splash: 'http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Zed_0.jpg' },
+  { id: 'Ziggs', tags: ['Mage'], splash: 'http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Ziggs_0.jpg' },
+  { id: 'Zilean', tags: ['Support', 'Mage'], splash: 'http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Zilean_0.jpg' },
+  { id: 'Bard', tags: ['Support', 'Mage'], splash: 'http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Bard_0.jpg' },
+  { id: 'Amumu', tags: ['Tank', 'Mage'], splash: 'http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Amumu_0.jpg' },
+  { id: 'Anivia', tags: ['Mage', 'Support'], splash: 'http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Anivia_0.jpg' },
+  { id: 'Zac', tags: ['Tank', 'Fighter'], splash: 'http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Zac_0.jpg' },
+  { id: 'undefined' },
+];*/
+
+
+// console.log('Rol de Zyra: ', array[4].tags[0]);
+// const arrPorTags = array.filter(filtrarPorTags);
+
+// funcion de filtrar inconclusa
+const arrPorTags = arrObjetos.filter(filtrarPorTags);
+// crearTemplate(arrPorTags);
+
+console.log('Array Filtrado\n', arrPorTags);
