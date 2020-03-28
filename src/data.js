3a671895b6a60d6ac70ffccebe753f5de6c0d560
Claudia Cortez Bocangel <60887205+claudiacortezb@users.noreply.github.com>
2020-03-27 20:31:24 -0500

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,15 +1,10 @@
 //  Funcionalidad en data
 //  muestro la info
-export const showInfo = (pokes,input) => {
-  const inputVal = input.value;
-  let info = '';
-  pokes.forEach((poke) => {
-    if (inputVal === poke.name) {
-      info = `
+export const showAllData = poke => `
               <p class="namePok"> ${poke.name}</p>
               <section id=firstRow>
-                <div id="imgCont">
-                    <span class="circle ft">${poke.num}</span>
+                <div class="imgCont">
+                    <span class="circle">${poke.num}</span>
                     <img src="${poke.img}" alt="pokemonImage">
                 </div>
                 <div id="s2info" class="infoCont">
@@ -26,22 +21,15 @@ export const showInfo = (pokes,input) => {
                     <p id="about" class="infoCont">${poke.about}</p>
                </section>
               `;
-    }
-  });
-  if (info) {
-    return info;
-  }
-  return 'El nombre ingresado no es correcto';
-};
-export const showAllData = (data) => {
-  return `
-        <div id="imgCont" class="item">
-            <span class="circle ft">${data.num}</span>
+// Muestra solo un poco
+export const showInfo = data => `
+        <div class="imgCont">
+            <span class="circle">${data.num}</span>
             <img src="${data.img}" alt="pokemonImage">
             <span> ${data.name}</span>
+            <p id="${data.num}" class="more">More</p>
         </div>
         `;
-};
 // Ordena Alfabeticamente
 export const orderBy = (poke, order) => {
   let arrSort = [];
@@ -54,13 +42,12 @@ export const orderBy = (poke, order) => {
       if (nameA > nameB) {
         return 1;
       }
-      else {
-        if (nameA < nameB) {
-          return -1;
-        }
-        {
-          return 0;
-        }
+
+      if (nameA < nameB) {
+        return -1;
+      }
+      {
+        return 0;
       }
       /* return (nameA > nameB) ? 1 : ((nameA < nameB) ? -1 : 0); */
     }
@@ -68,13 +55,12 @@ export const orderBy = (poke, order) => {
       if (nameA > nameB) {
         return -1;
       }
-      else {
-        if (nameA < nameB) {
-          return 1;
-        }
-        {
-          return 0;
-        }
+
+      if (nameA < nameB) {
+        return 1;
+      }
+      {
+        return 0;
       }
     }
     //  numero descendente
@@ -83,13 +69,12 @@ export const orderBy = (poke, order) => {
       if (numA > numB) {
         return -1;
       }
-      else {
-        if (numA < numB) {
+
+      if (numA < numB) {
         return 1;
-        }
-        {
-         return 0;
-        }
+      }
+      {
+        return 0;
       }
     }
   });
@@ -100,4 +85,17 @@ export const typeFilter = (poke, tipo) => {
   // eslint-disable-next-line no-shadow
   const arrFilt = poke.filter(poke => poke.type.includes(tipo)); // retorna un boolean
   return arrFilt;
+};
+// Buscador
+export const searcher = (data, value) => {
+  let info = '';
+  data.forEach((poke) => {
+    if (value === poke.name || value === poke.num) {
+      info = showAllData(poke);
+    }
+  });
+  if (info) {
+    return info;
+  }
+  return 'El nombre ingresado no es correcto';
 };
