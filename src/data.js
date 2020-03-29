b6aa98c5e0d06900ed38b78d33c844185dc0215e
Lemi-Torres <lmt.2020v@gmail.com>
2020-03-29 10:53:50 -0500

    h1,h2,h3

--- src/data.js
+++ src/data.js
@@ -1,6 +1,3 @@
-/* eslint-disable no-undef */
-/* eslint-disable no-plusplus */
-/* eslint-disable import/prefer-default-export */
 // export const example = () => 'example';
 // export const anotherExample = () => 'OMG';
 
@@ -58,15 +55,3 @@ export const filterData = (data, condition) => {
   });
   return filtering;
 };
-
-/*export const filterData = (data, condition) => {
-  const filteredByRole = [];
-  for (let i = 0; i < data.length; i++) {
-    for (let e = 0; e < data[i].tags.length; e++) {
-      if (data[i].tags[e] === condition) {
-        filteredByRole.push(data[i]);
-      }
-    }
-  }
-  return filteredByRole;
-}; */
--- src/main.js
+++ src/main.js
@@ -1,10 +1,5 @@
-/* eslint-disable import/named */
 /* eslint-disable no-undef */
-/* eslint-disable max-len */
-/* eslint-disable spaced-comment */
-/* eslint-disable no-multiple-empty-lines */
 /* eslint-disable import/extensions */
-/* eslint-disable no-plusplus */
 // import data from './data/atletas/atletas.js';
 // import data from './data/pokemon/pokemon.js';
 // console.log(example,data);
@@ -20,15 +15,19 @@ const arrObjetos = Object.values(allChampion);
 const crearTemplate = (arr) => {
   arr.forEach((champion) => {
     const newElement = document.createElement('div');
-    newElement.classList.add('class-div');
     const img = document.createElement('img');
-    img.classList.add('class-img');
     const p = document.createElement('p');
+    const h3 = document.createElement('h3');
+    newElement.classList.add('class-div');
+    img.classList.add('class-img');
     p.classList.add('class-name');
-    newElement.appendChild(img);
-    newElement.appendChild(p);
+    h3.classList.add('class-tags');
     img.src = `${champion.splash}`;
     p.innerHTML = `${champion.name}`;
+    h3.innerHTML = `${champion.tags}`;
+    newElement.appendChild(img);
+    newElement.appendChild(p);
+    newElement.appendChild(h3);
     const container = document.getElementById('container');
     container.appendChild(newElement);
   });
@@ -79,55 +78,3 @@ tank.addEventListener('click', () => {
   container.innerHTML = '';
   crearTemplate(filterData(arrObjetos, 'Tank'));
 });
-
-
-//opcion2
-/*const crearTemplate = (arr) => {
-  const all = [];
-  for (let i = 0; i < arr.length; i++) {
-    all.push(`
-      <div class="class-div">
-      <img src="${arr[i].splash}" class="class-img" alt="Imagen de ${arr[i].name}">
-      <p class="class-name">${arr[i].name}</p>
-      </div>`);
-  }
-  return all.join('');
-};
-
-document.getElementById('box').innerHTML = crearTemplate(arrObjetos);
-
-document.getElementById('order').addEventListener('change', () => {
-  const order = document.getElementById('order').value;
-  document.getElementById('box').innerHTML = crearTemplate(orderChampions(arrObjetos, 'name', order));
-});*/
-
-//opcion1 original
-/*document.getElementById('order').addEventListener('change', () => {
-  document.getElementById('box').innerHTML = crearTemplate(orderChampions(arrObjetos, 'name', document.getElementById('order').value));
-});*/
-
-
-/*document.getElementById('fighter').addEventListener('click', () => {
-  document.getElementById('box').innerHTML = crearTemplate(filterData(arrObjetos, 'Fighter'));
-});*/
-
-/*document.getElementById('tank').addEventListener('click', () => {
-  document.getElementById('box').innerHTML = crearTemplate(filterData(arrObjetos, 'Tank'));
-});*/
-
-/*document.getElementById('mage').addEventListener('click', () => {
-  document.getElementById('box').innerHTML = crearTemplate(filterData(arrObjetos, 'Mage'));
-});
-
-document.getElementById('assassin').addEventListener('click', () => {
-  document.getElementById('box').innerHTML = crearTemplate(filterData(arrObjetos, 'Assassin'));
-});
-
-document.getElementById('support').addEventListener('click', () => {
-  document.getElementById('box').innerHTML = crearTemplate(filterData(arrObjetos, 'Support'));
-});
-
-document.getElementById('marksman').addEventListener('click', () => {
-  document.getElementById('box').innerHTML = crearTemplate(filterData(arrObjetos, 'Marksman'));
-});*/
-
