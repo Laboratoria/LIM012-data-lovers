608243d278a45661013eb6502e453bdd8e198f80
GuadalupeLarios <gdlp.larios@gmail.com>
2020-03-29 20:41:08 -0500

    2903

--- src/data.js
+++ src/data.js
@@ -1,5 +1,26 @@
-// estas funciones son de ejemplo
+export const ordenar = (ordenarData, seleccion) => {
+  let resultado;
+  if (seleccion === 'a-z'){
+    resultado = ordenarData.sort((a, b) => {
+      if (a.nombre > b.nombre) {
+        return 1;
+      }
+      if (a.nombre < b.nombre) {
+        return -1;
+      }
+      return 0;
+    });
+  }else {
+    resultado = ordenarData.sort((a, b) => {
+      if (a.nombre < b.nombre) {
+        return 1;
+      }
+      if (a.nombre > b.nombre) {
+        return -1;
+      }
+      return 0;
+    });
+  }
 
-export const example = () => 'example';
-
-export const anotherExample = () => 'OMG';
+return resultado;
+};
--- src/main.js
+++ src/main.js
@@ -1,6 +1,85 @@
-import { example } from './data.js';
-// import data from './data/atletas/atletas.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+import { ordenar } from './data.js';
+import data from './data/atletas/atletas.js';
 
-console.log(example, data);
+const barraInicio = document.getElementById('barraInicio');
+const datageneral = document.getElementById('datageneral');
+const barraMedallero = document.getElementById('barraMedallero');
+const filtroAz = document.getElementById('filtroAz');
+
+document.getElementById('inicio').addEventListener('click', () => {
+  barraInicio.classList.remove('hide');
+  datageneral.classList.add('hide');
+  barraMedallero.classList.add('hide');
+  filtroAz.classList.add('hide');
+});
+
+document.getElementById('medallero').addEventListener('click', () => {
+  datageneral.classList.add('hide');
+  barraInicio.classList.add('hide');
+  barraMedallero.classList.remove('hide');
+});
+
+document.getElementById('btnSidney').addEventListener('click', () => {
+  barraMedallero.classList.remove('hide');
+});
+
+document.getElementById('btnAtenas').addEventListener('click', () => {
+  barraMedallero.classList.remove('hide');
+});
+
+document.getElementById('btnBeijing').addEventListener('click', () => {
+  barraMedallero.classList.remove('hide');
+});
+
+document.getElementById('btnLondres').addEventListener('click', () => {
+  barraMedallero.classList.remove('hide');
+});
+
+document.getElementById('btnRio').addEventListener('click', () => {
+  barraMedallero.classList.remove('hide');
+});
+
+document.getElementById('atletas').addEventListener('click', () => {
+  datageneral.classList.remove('hide');
+  barraInicio.classList.add('hide');
+  filtroAz.classList.remove('hide');
+
+  const baseAtletas = (deportistas) => {
+
+    let contenedor = '';
+
+    const listaEncabezado = `
+    <div class="encabezado">
+        <div class="columna">Nombre</div>
+        <div class="column">Pais</div>
+        <div class="column">Deporte</div>
+        <div class="colum">Peso</div>
+        <div class="colum">Altura</div>
+        <div class="colum">Género</div>
+     </div>
+     `;
+
+    deportistas.forEach((item) => {
+      const lista = `
+
+    <div class="fila">
+      <div class="columna">${item.nombre}</div>
+      <div class="column">${item.equipo}</div>
+      <div class="column">${item.deporte}</div>
+      <div class="colum">${item.peso}</div>
+      <div class="colum">${item.altura}</div>
+      <div class="colum">${item.genero}</div>
+    </div>
+  `;
+      contenedor += lista;
+    });
+    datageneral.innerHTML = listaEncabezado + contenedor;
+  };
+
+  baseAtletas(data.atletas);
+
+filtroAz.addEventListener('change', () => {
+    const select = filtroAz.value;
+    baseAtletas(ordenar(data.atletas, select));
+});
+});
