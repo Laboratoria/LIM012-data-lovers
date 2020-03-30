e705b822fbe94b74772b5f127e849b2d7179cc67
claudiacortezb <claudia.cboc@gmail.com>
2020-03-29 22:27:10 -0500

    Ya se muestra la info al clickear

--- src/data.js
+++ src/data.js
@@ -1,23 +1,10 @@
 //  Funcionalidad en data
 //  muestro la info
-<<<<<<< HEAD
-export const showInfo = (pokes,input) => {
-  const inputVal = input.value;
-  let info = '';
-  pokes.forEach((poke) => {
-    if (inputVal === poke.name) {
-      info = `
-              <p class="namePok"> ${poke.name}</p>
-              <section id=firstRow>
-                <div id="imgCont">
-                    <span class="circle ft">${poke.num}</span>
-=======
 export const showAllData = poke => `
               <p class="namePok"> ${poke.name}</p>
               <section id=firstRow>
                 <div class="imgCont">
                     <span class="circle">${poke.num}</span>
->>>>>>> ad6771bc932222bf7784eebde4133cc835ab9b3a
                     <img src="${poke.img}" alt="pokemonImage">
                 </div>
                 <div id="s2info" class="infoCont">
@@ -28,34 +15,12 @@ export const showAllData = poke => `
               </section>
                     <div id="hW" class="infoCont">
                       <p>Height:<span>${poke.size.height}</span></p>
-<<<<<<< HEAD
                       <p>Weight:<span>${poke.size.weight}</span></p>  
-=======
-                      <p>Weight:<span>${poke.size.weight}</span></p>
->>>>>>> ad6771bc932222bf7784eebde4133cc835ab9b3a
                       <p>Base Attack:<span>${poke.stats['base-attack']}</span></p>
                     </div>
                     <p id="about" class="infoCont">${poke.about}</p>
                </section>
               `;
-<<<<<<< HEAD
-    }
-  });
-  if (info) {
-    return info;
-  }
-  return 'El nombre ingresado no es correcto';
-};
-export const showAllData = (data) => {
-  return `
-        <div id="imgCont" class="item">
-            <span class="circle ft">${data.num}</span>
-            <img src="${data.img}" alt="pokemonImage">
-            <span> ${data.name}</span>
-        </div>
-        `;
-};
-=======
 // Muestra solo un poco
 export const showInfo = data => `
         <div class="imgCont">
@@ -65,7 +30,6 @@ export const showInfo = data => `
             <p id="${data.num}" class="more">More</p>
         </div>
         `;
->>>>>>> ad6771bc932222bf7784eebde4133cc835ab9b3a
 // Ordena Alfabeticamente
 export const orderBy = (poke, order) => {
   let arrSort = [];
@@ -78,45 +42,26 @@ export const orderBy = (poke, order) => {
       if (nameA > nameB) {
         return 1;
       }
-<<<<<<< HEAD
-      else {
-        if (nameA < nameB) {
-          return -1;
-        }
-        {
-          return 0;
-        }
-=======
 
       if (nameA < nameB) {
         return -1;
       }
       {
         return 0;
->>>>>>> ad6771bc932222bf7784eebde4133cc835ab9b3a
       }
+
       /* return (nameA > nameB) ? 1 : ((nameA < nameB) ? -1 : 0); */
     }
     if (order === 'desc') {
       if (nameA > nameB) {
         return -1;
       }
-<<<<<<< HEAD
-      else {
-        if (nameA < nameB) {
-          return 1;
-        }
-        {
-          return 0;
-        }
-=======
 
       if (nameA < nameB) {
         return 1;
       }
       {
         return 0;
->>>>>>> ad6771bc932222bf7784eebde4133cc835ab9b3a
       }
     }
     //  numero descendente
@@ -125,22 +70,12 @@ export const orderBy = (poke, order) => {
       if (numA > numB) {
         return -1;
       }
-<<<<<<< HEAD
-      else {
-        if (numA < numB) {
-        return 1;
-        }
-        {
-         return 0;
-        }
-=======
 
       if (numA < numB) {
         return 1;
       }
       {
         return 0;
->>>>>>> ad6771bc932222bf7784eebde4133cc835ab9b3a
       }
     }
   });
@@ -151,10 +86,6 @@ export const typeFilter = (poke, tipo) => {
   // eslint-disable-next-line no-shadow
   const arrFilt = poke.filter(poke => poke.type.includes(tipo)); // retorna un boolean
   return arrFilt;
-<<<<<<< HEAD
-};
-=======
-<<<<<<< HEAD
 };
 // Buscador
 export const searcher = (data, value) => {
@@ -169,7 +100,3 @@ export const searcher = (data, value) => {
   }
   return 'El nombre ingresado no es correcto';
 };
-=======
-};
->>>>>>> 10d78910f7b3c0856157af67a2cc55f683689a00
->>>>>>> ad6771bc932222bf7784eebde4133cc835ab9b3a
--- src/main.js
+++ src/main.js
@@ -2,19 +2,11 @@
 //  {} cuando solo llamas a una funcion determinada no a toda la hoja
 import data from './data/pokemon/pokemon.js';
 import {
-<<<<<<< HEAD
-  showInfo, showAllData, orderBy, typeFilter,
-} from './data.js';
-
-const inputSearchElem = document.getElementById('inputSearch');
-const pokemones = data.pokemon;
-=======
   searcher, showInfo, orderBy, typeFilter,
 } from './data.js';
 
 const pokemones = data.pokemon;
 const inputSearchElem = document.getElementById('inputSearch');
->>>>>>> ad6771bc932222bf7784eebde4133cc835ab9b3a
 const searchIconElem = document.getElementById('searchIcon');
 const screen1Elem = document.getElementById('screen1');
 const screen2Elem = document.getElementById('screen2');
@@ -23,28 +15,6 @@ const screen4Elem = document.getElementById('screen4');
 const select1Elem = document.getElementById('selectOrder');
 const select2Elem = document.getElementById('selectType');
 const s3dataElem = document.getElementById('s3data');
-<<<<<<< HEAD
-// const s3dataElem = document.getElementById('s3data');
-// document.getElementsByTagName(select[]);
-const mHome = document.getElementById('home');
-const mPokedex = document.getElementById('pokedex');
-const mCompare = document.getElementById('compare');
-// Segunda Pantalla -Buscador
-/* let fun=(pokemones,data)=>{
-  let je=[];
-  for(let i in pokemones){
-    for(let j in data){
-  je.push(pokemones[i][data][j].name)}
-  }
-  console.log(je);}
-fun(pokemones,'special-attack'); */
-
-
-searchIconElem.addEventListener('click', () => {
-  screen1Elem.style.display = 'none';
-  screen2Elem.style.display = 'block';
-  screen2Elem.innerHTML = `${showInfo(pokemones, inputSearchElem)}`;
-=======
 const mHome = document.querySelector('#home');
 const mPokedex = document.querySelector('#pokedex');
 const mCompare = document.querySelector('#compare');
@@ -55,7 +25,6 @@ searchIconElem.addEventListener('click', () => {
   screen2Elem.style.display = 'block';
   const inputVal = inputSearchElem.value;
   screen2Elem.innerHTML = `${searcher(pokemones, inputVal)}`;
->>>>>>> ad6771bc932222bf7784eebde4133cc835ab9b3a
 });
 // Menu boton Home
 mHome.addEventListener('click', () => {
@@ -70,18 +39,6 @@ mPokedex.addEventListener('click', () => {
   screen1Elem.style.display = 'none';
   screen2Elem.style.display = 'none';
   screen3Elem.style.display = 'flex';
-<<<<<<< HEAD
-  s3dataElem.innerHTML = `${pokemones.map(showAllData).join('')}`;
-});
-// Boton Select OrderBy
-select1Elem.addEventListener('change', () => {
-  const select1Value = select1Elem.value;
-  s3dataElem.innerHTML = `${(orderBy(pokemones, select1Value)).map(showAllData).join('')}`;
-});
-select2Elem.addEventListener('change', () => {
-  const select2Value = select2Elem.value.toLowerCase();
-  s3dataElem.innerHTML = `${(typeFilter(pokemones, select2Value)).map(showAllData).join('')}`;
-=======
   s3dataElem.innerHTML = `${pokemones.map(showInfo).join('')}`;
 });
 // Muestra Info al tocar al pokemon
@@ -101,7 +58,6 @@ select1Elem.addEventListener('change', () => {
 select2Elem.addEventListener('change', () => {
   const select2Value = select2Elem.value.toLowerCase();
   s3dataElem.innerHTML = `${(typeFilter(pokemones, select2Value)).map(showInfo).join('')}`;
->>>>>>> ad6771bc932222bf7784eebde4133cc835ab9b3a
 });
 
 mCompare.addEventListener('click', () => {
@@ -110,8 +66,4 @@ mCompare.addEventListener('click', () => {
   screen3Elem.style.display = 'none';
   screen4Elem.style.display = 'flex';
   // screen4Elem.innerHTML = `${showInfo(pokemones, inputSearchIconCElem)}`;
-<<<<<<< HEAD
 });
-=======
-});
->>>>>>> ad6771bc932222bf7784eebde4133cc835ab9b3a
