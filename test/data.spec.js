09f56d94f274ad37e59d647594f5450ee94372c7
Lourdes0896 <sam.lourdesmerino@gmail.com>
2020-03-30 19:02:18 -0500

    test de H1

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,23 +1,89 @@
-import { example, anotherExample } from '../src/data.js';
+// import data from '../src/data/lol/lol.js';
+import { ordenarCampeones } from '../src/data.js';
 
+const firstTestExpect = [
+  {
+    id: 'Nautilus',
+    key: '111',
+    name: 'Nautilus',
+    title: 'the Titan of the Depths',
+  },
+  {
+    id: 'Aatrox',
+    key: '266',
+    name: 'Aatrox',
+    title: 'the Darkin Blade',
+  },
+  {
+    id: 'Taliyah',
+    key: '163',
+    name: 'Taliyah',
+    title: 'the Stoneweaver',
+  },
+];
 
-describe('example', () => {
+const firstTestoBe = [
+  {
+    id: 'Aatrox',
+    key: '266',
+    name: 'Aatrox',
+    title: 'the Darkin Blade',
+  },
+  {
+    id: 'Nautilus',
+    key: '111',
+    name: 'Nautilus',
+    title: 'the Titan of the Depths',
+  },
+  {
+    id: 'Taliyah',
+    key: '163',
+    name: 'Taliyah',
+    title: 'the Stoneweaver',
+  },
+];
+
+const secondTestoBe = [
+  {
+    id: 'Taliyah',
+    key: '163',
+    name: 'Taliyah',
+    title: 'the Stoneweaver',
+  },
+  {
+    id: 'Nautilus',
+    key: '111',
+    name: 'Nautilus',
+    title: 'the Titan of the Depths',
+  },
+  {
+    id: 'Aatrox',
+    key: '266',
+    name: 'Aatrox',
+    title: 'the Darkin Blade',
+  },
+];
+
+describe('ordenarCampeones', () => {
   it('is a function', () => {
-    expect(typeof example).toBe('function');
+    expect(typeof ordenarCampeones).toBe('function');
   });
 
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
+  it('Debe retornar los campeones ordenados de a-z', () => {
+    expect(ordenarCampeones(firstTestExpect, 'az')).toEqual(firstTestoBe);
+  });
+  it('Debe retornar los campeones ordenados de z-a', () => {
+    expect(ordenarCampeones(firstTestExpect, 'za')).toEqual(secondTestoBe);
   });
 });
 
 
-describe('anotherExample', () => {
-  it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
-  });
+// describe('anotherExample', () => {
+//   it('is a function', () => {
+//     expect(typeof anotherExample).toBe('function');
+//   });
 
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
-  });
-});
+//   it('returns `anotherExample`', () => {
+//     expect(anotherExample()).toBe('OMG');
+//   });
+// });
