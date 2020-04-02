90b0b65d5533af7fa5ba07fcc8ac6f900a9dfb99
Sonia Seuc <60791273+soniaseuc@users.noreply.github.com>
2020-04-01 19:16:47 -0500

    02-04 Mier funcion calculo agregado(promedio x campeon) terminada codigo repetitivo

--- src/data.js
+++ src/data.js
@@ -24,18 +24,17 @@ export const sortData = (data, sortBy, sortOrder) => {
       if (a.name > b.name) {
         return -1;
       }
-
       return 0;
     });
   }
-  /*if (sortOrder === 'todos') {
+  /* if (sortOrder === 'todos') {
     newArray.sort((a, b) => {
       if (a.name > b.name) {
         return 1;
       }
       return -1;
     });
-  }*/
+  } */
   return newArray;
 };
 
@@ -52,3 +51,48 @@ export const filterData = (data, condition) => {
   });
   return filtering;
 };
+
+export const filterAssassin = (data) => {
+  const filterOnlyAssassin = data.filter(champion => champion.tags.includes('Assassin'));
+  const reduceHpAssassin = filterOnlyAssassin.reduce((total, next) => total + next.stats.hp, 0);
+  const avgHpAssassin = reduceHpAssassin / filterOnlyAssassin.length;
+  return avgHpAssassin;
+};
+
+export const filterFighter = (data) => {
+  const filterOnlyFighter = data.filter(champion => champion.tags.includes('Fighter'));
+  const reducehpfighter = filterOnlyFighter.reduce((total, next) => total + next.stats.hp, 0);
+  const avghpfighter = reducehpfighter / filterOnlyFighter.length;
+  return avghpfighter;
+};
+
+export const filterMage = (data) => {
+  const filterOnlyMage = data.filter(champion => champion.tags.includes('Mage'));
+  const reduceHpMage = filterOnlyMage.reduce((total, next) => total + next.stats.hp, 0);
+  const avgHpMage = reduceHpMage / filterOnlyMage.length;
+  return avgHpMage;
+};
+export const filterMarksman = (data) => {
+  const filterOnlyMarksman = data.filter(champion => champion.tags.includes('Marksman'));
+  const reduceHpMarksman = filterOnlyMarksman.reduce((total, next) => total + next.stats.hp, 0);
+  const avgHpMarksman = reduceHpMarksman / filterOnlyMarksman.length;
+  return avgHpMarksman;
+};
+
+export const filterSupport = (data) => {
+  const filterOnlySupport = data.filter(champion => champion.tags.includes('Support'));
+  const reduceHpSupport = filterOnlySupport.reduce((total, next) => total + next.stats.hp, 0);
+  const avgHpSupport = reduceHpSupport / filterOnlySupport.length;
+  return avgHpSupport;
+};
+export const filterTank = (data) => {
+  const filterOnlyTank = data.filter(champion => champion.tags.includes('Tank'));
+  const reduceHpTank = filterOnlyTank.reduce((total, next) => total + next.stats.hp, 0);
+  const avgHpTank = reduceHpTank / filterOnlyTank.length;
+  return avgHpTank;
+};
+
+// const filterfighter = arrObjetos.filter(champion => champion.tags.includes('Fighter'));
+// const reducehpfighter = filterfighter.reduce((total, next) => total + next.stats.hp, 0);
+// const avghpfighter = reducehpfighter / filterfighter.length;
+// console.log(('promedio de hp de los Fighter'), avghpfighter);
--- src/main.js
+++ src/main.js
@@ -8,12 +8,15 @@
 //  AQUI MANEJAMOS EL DOOM
 
 import data from './data/lol/lol.js';
-import { sortData, filterData } from './data.js';
+import {
+  sortData, filterData, filterFighter, filterMage,
+  filterAssassin, filterMarksman, filterSupport, filterTank,
+} from './data.js';
 
 const allChampion = data.data;
 const arrObjetos = Object.values(allChampion);
-console.log(allChampion);
-console.log(arrObjetos);
+// console.log(allChampion);
+// console.log(arrObjetos);
 
 
 const crearTemplate = (arr) => {
@@ -38,54 +41,95 @@ const crearTemplate = (arr) => {
 };
 crearTemplate(arrObjetos);
 
+// CREAR PARRAFO PARA MOSTRAR ESTADISTICA DE CALCULO AGREGADO
+const pElement = document.createElement('p');
+pElement.classList.add('class-name');
+
 // ORDENAR DATA
 const orderAz = document.getElementById('orderAz');
 orderAz.addEventListener('click', () => {
   container.innerHTML = '';
+  pElement.innerHTML = '';
   crearTemplate(sortData(arrObjetos, 'name', 'ascendente'));
 });
 
 const orderZa = document.getElementById('orderZa');
 orderZa.addEventListener('click', () => {
   container.innerHTML = '';
+  pElement.innerHTML = '';
   crearTemplate(sortData(arrObjetos, 'name', 'descendente'));
 });
-
 
 // FILTRAR DATA
 const assassin = document.getElementById('assassin');
 assassin.addEventListener('click', () => {
   container.innerHTML = '';
+
+  pElement.innerHTML = '';
+  pElement.innerHTML = `Assassin Average HP: ${filterAssassin(arrObjetos)}`;
+  const average = document.getElementById('average');
+  average.appendChild(pElement);
+
   crearTemplate(filterData(arrObjetos, 'Assassin'));
 });
 
 const fighter = document.getElementById('fighter');
 fighter.addEventListener('click', () => {
   container.innerHTML = '';
+
+  pElement.innerHTML = '';
+  pElement.innerHTML = `Fighter Average HP: ${filterFighter(arrObjetos)}`;
+  const average = document.getElementById('average');
+  average.appendChild(pElement);
+
   crearTemplate(filterData(arrObjetos, 'Fighter'));
 });
 
 const mage = document.getElementById('mage');
 mage.addEventListener('click', () => {
   container.innerHTML = '';
+
+  pElement.innerHTML = '';
+  pElement.innerHTML = `Mage Average Hp: ${filterMage(arrObjetos)}`;
+  const average = document.getElementById('average');
+  average.appendChild(pElement);
+
   crearTemplate(filterData(arrObjetos, 'Mage'));
 });
 
 const marksman = document.getElementById('marksman');
 marksman.addEventListener('click', () => {
   container.innerHTML = '';
+
+  pElement.innerHTML = '';
+  pElement.innerHTML = `Marksman Average Hp: ${filterMarksman(arrObjetos)}`;
+  const average = document.getElementById('average');
+  average.appendChild(pElement);
+
   crearTemplate(filterData(arrObjetos, 'Marksman'));
 });
 
 const support = document.getElementById('support');
 support.addEventListener('click', () => {
   container.innerHTML = '';
+
+  pElement.innerHTML = '';
+  pElement.innerHTML = `Support Average Hp: ${filterSupport(arrObjetos)}`;
+  const average = document.getElementById('average');
+  average.appendChild(pElement);
+
   crearTemplate(filterData(arrObjetos, 'Support'));
 });
 
 const tank = document.getElementById('tank');
 tank.addEventListener('click', () => {
   container.innerHTML = '';
+
+  pElement.innerHTML = '';
+  pElement.innerHTML = `Tank Average Hp: ${filterTank(arrObjetos)}`;
+  const average = document.getElementById('average');
+  average.appendChild(pElement);
+
   crearTemplate(filterData(arrObjetos, 'Tank'));
 });
 
@@ -108,3 +152,18 @@ li.forEach((el) => {
     el.classList.add('active');
   });
 });
+
+
+// const filterfighter = arrObjetos.filter(champeon => champeon.tags.includes('Fighter'));
+// console.log(('Campeones filtrados que son Fighter:'), filterfighter);
+
+// const reducehpfighter = filterfighter.reduce((total, next) => total + next.stats.hp, 0);
+// console.log(('suma de hp de los Fighter'), reducehpfighter);
+
+// const averagehpfighter = filterfighter.reduce((total, next) =>
+// total + next.stats.hp, 0) / filterfighter.length;
+// console.log(('promedio de hp de los Fighter'), averagehpfighter);
+
+// const averagemsfighter = filterfighter.reduce((total, next) =>
+// total + next.stats.movespeed, 0) / filterfighter.length;
+// console.log(('promedio de movespeed de los Fighter'), averagemsfighter);
