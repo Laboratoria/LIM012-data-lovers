21e20183bf9fb361b29de283b0020e7a9f2cca83
Guadalupe Larios <60905476+GuadalupeLarios@users.noreply.github.com>
2020-04-12 00:33:09 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
-import { ordenar, filtroPorArchery } from './data.js';
+import { ordenar } from './data.js';
 import data from './data/atletas/atletas.js';
 
-const barraInicio = document.getElementById('barraInicio');
-window.alert('Quedate en casa. Las autoridades estan haciendo su trabajo en relación al COVID-19, pero necesitan de la disciplina de todos. No salgas de casa a menos que tengas una urgencia personal o familiar, mientras tanto puedes disfrutar de esta página.');
+window.alert('Quedate en casa. Las autoridades estan haciendo su trabajo en vista del brote de COVID-19, pero necesitan de la disciplina de todos. No salgas de casa a menos que tengas una urgencia personal o familiar, mientras tanto puedes disfrutar de esta página.');
 
 const barraInicio = document.getElementById('contenedorInicio');
 const datageneral = document.getElementById('datageneral');
@@ -11,8 +10,6 @@ const botonTop = document.querySelector('.botonTop');
 const contenedorAtletas = document.getElementById('contenedorAtletas');
 const barraSedes = document.getElementById('barraSedes');
 const filtroAz = document.getElementById('filtroAz');
-const barraDeportes = document.getElementById('barraDeportes');
-const dataDeportes = document.getElementById('dataDeportes');
 
 const scroll = () => {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -37,8 +34,6 @@ document.getElementById('inicio').addEventListener('click', () => {
   barraInicio.classList.remove('hide');
   datageneral.classList.add('hide');
   barraMedallero.classList.add('hide');
-  filtroAz.classList.add('hide');
-  barraDeportes.classList.add('hide');
   contenedorAtletas.classList.add('hide');
   barraSedes.classList.add('hide');
 });
@@ -74,7 +69,6 @@ document.getElementById('btnRio').addEventListener('click', () => {
 document.getElementById('atletas').addEventListener('click', () => {
   datageneral.classList.remove('hide');
   barraInicio.classList.add('hide');
-  filtroAz.classList.remove('hide');
   barraMedallero.classList.add('hide');
   contenedorAtletas.classList.remove('hide');
   barraSedes.classList.add('hide');
@@ -83,6 +77,7 @@ document.getElementById('atletas').addEventListener('click', () => {
     let contenedor = '';
 
     const listaEncabezado = `
+
     <div class="encabezado">
         <div class="columna">Nombre</div>
         <div class="column">Pais</div>
@@ -95,7 +90,6 @@ document.getElementById('atletas').addEventListener('click', () => {
 
     deportistas.forEach((item) => {
       const lista = `
-
     <div class="fila">
       <div class="columna">${item.nombre}</div>
       <div class="column">${item.equipo}</div>
@@ -104,7 +98,7 @@ document.getElementById('atletas').addEventListener('click', () => {
       <div class="colum">${item.altura}</div>
       <div class="colum">${item.genero}</div>
     </div>
-  `;
+    `;
       contenedor += lista;
     });
     datageneral.innerHTML = listaEncabezado + contenedor;
@@ -118,50 +112,6 @@ document.getElementById('atletas').addEventListener('click', () => {
   });
 });
 
-document.getElementById('deportes').addEventListener('click', () => {
-  datageneral.classList.add('hide');
-  barraInicio.classList.add('hide');
-  filtroAz.classList.add('hide');
-  barraDeportes.classList.remove('hide');
-});
-
-
-const deportes = (deportistas) => {
-  let contenedorSports = '';
-
-  const listaEncabezadoSports = `
-    <div class="encabezado">
-        <div class="columna">Nombre</div>
-        <div class="column">Pais</div>
-        <div class="column">Deporte</div>
-        <div class="colum">Peso</div>
-        <div class="colum">Altura</div>
-        <div class="colum">Género</div>
-     </div>
-     `;
-
-  deportistas.forEach((item) => {
-    const lista1 = `
-    <div class="fila">
-      <div class="columna">${item.nombre}</div>
-      <div class="column">${item.equipo}</div>
-      <div class="column">${item.deporte}</div>
-      <div class="colum">${item.peso}</div>
-      <div class="colum">${item.altura}</div>
-      <div class="colum">${item.genero}</div>
-    </div>
-  `;
-    contenedorSports += lista1;
-  });
-  dataDeportes.innerHTML = listaEncabezadoSports + contenedorSports;
-};
-deportes(data.atletas);
-document.getElementById('btnar').addEventListener('click', () => {
-  barraInicio.classList.add('hide');
-  filtroAz.classList.add('hide');
-  barraDeportes.classList.add('hide');
-  dataDeportes.classList.remove('hide');
-  deportes(filtroPorArchery(data.atletas));
 document.getElementById('sedes').addEventListener('click', () => {
   barraInicio.classList.add('hide');
   datageneral.classList.add('hide');
